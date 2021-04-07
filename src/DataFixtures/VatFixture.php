401705deee7ad2f63473cc54113926df120d6d37
U-EU\patykmar <patykmar@WL364163.eu.tieto.com>
2021-04-07 09:20:34 +0200

    Update VatFixture.php after merge from Gitlab server

--- src/DataFixtures/VatFixture.php
+++ src/DataFixtures/VatFixture.php
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\DataFixtures;
-
-use App\Entity\Vat;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
-
-class VatFixture extends Fixture
-{
-    public const NO_VAT = 'no-vat';
-    public  const VAT_21 = 'vat-21';
-
-    public function load(ObjectManager $manager)
-    {
-        $vatNo = new Vat();
-        $vatNo->setName("Bez DPH")
-            ->setMultiplier(1.0)
-            ->setPercent(0)
-            ->setIsDefault(TRUE);
-
-        $vat21 = new Vat();
-        $vat21->setName("21%")
-            ->setMultiplier(1.21)
-            ->setPercent(21)
-            ->setIsDefault(FALSE);
-
-        $this->addReference(self::NO_VAT,$vatNo);
-        $this->addReference(self::VAT_21,$vat21);
-
-        $manager->persist($vatNo);
-        $manager->persist($vat21);
-
-        $manager->flush();
-    }
-}
-=======
 <?php
 
 namespace App\DataFixtures;
@@ -72,4 +34,3 @@ class VatFixture extends Fixture
         $manager->flush();
     }
 }
->>>>>>> a18ee548b701200a5a978c4861964036ab0182b8
