bb197591b3c1cc8b7e006eaf585bc1ae940ef9c8
patykmar <patyk.m@gmail.com>
2021-04-11 13:44:43 +0200

    Update handling VAT

--- src/EventSubscriber/VatSubscriber.php
+++ src/EventSubscriber/VatSubscriber.php
@@ -1,10 +1,10 @@
 <?php
 
-
 namespace App\EventSubscriber;
 
-
 use App\Entity\Vat;
+use App\Repository\VatRepository;
+use ContainerXtzauUv\getConsole_Command_EventDispatcherDebugService;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityDeletedEvent;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
@@ -13,11 +13,12 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class VatSubscriber implements EventSubscriberInterface
 {
-    /**
-     * @var EntityManagerInterface
-     */
+
+    /** @var EntityManagerInterface */
     private EntityManagerInterface $entityManager;
 
+    /** @var VatRepository */
+    private $repo;
 
     /**
      * VatSubscriber constructor.
@@ -26,6 +27,7 @@ class VatSubscriber implements EventSubscriberInterface
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+        $this->repo = $this->entityManager->getRepository(Vat::class);
     }
 
     /**
@@ -47,8 +49,7 @@ class VatSubscriber implements EventSubscriberInterface
         if ($entity instanceof Vat) {
             // if user select some item to default set rest of items to false
             if ($entity->getIsDefault())
-                $this->updateDefaultItem();
-
+                $this->updateDefaultItem($entity);
         }
 
         unset($entity);
@@ -60,7 +61,12 @@ class VatSubscriber implements EventSubscriberInterface
         if ($entity instanceof Vat) {
             // if user select some item to default set rest of items to false
             if ($entity->getIsDefault())
-                $this->updateDefaultItem();
+                $this->updateDefaultItem($entity);
+
+            // if table is empty set default true by default
+            $rowCount = $this->repo->getCount();
+            if ($rowCount == 0)
+                $entity->setIsDefault(true);
         }
 
         unset($entity);
@@ -71,21 +77,54 @@ class VatSubscriber implements EventSubscriberInterface
         $entity = $event->getEntityInstance();
         if ($entity instanceof Vat) {
             //TODO: osetri mazani vychozi hodnoty
-            dd($entity);
+            // are you try delete row with default value ?
+            if ($entity->getIsDefault()) {
+                // are there any other rows ?
+                if ($this->repo->getCount() > 0) {
+                    $this->setIsDefaultToTrueOnSomeRow($entity);
+                }
+            }
+            #dd($entity);
         }
         unset($entity);
     }
-
 
     /**
      * Set isDefault to false for each rows
      * @throws \Doctrine\ORM\ORMException
      */
-    private function updateDefaultItem(): void
+    private function updateDefaultItem(Vat $entity): void
     {
-        // set all default
-        $this->entityManager->getRepository(Vat::class)
-            ->unsetDefaultAll();
+        /** @var Vat */
+        $whoIsDefault = $this->repo->getDefaultEntity();
+
+        // if $whoIsDefault is null no entity is default
+        if (is_null($whoIsDefault))
+            return;
+
+        // in case you are editing the default entry don't do that
+        if ($entity->getId() != $whoIsDefault->getId()) {
+            // set all default
+            $this->repo->unsetDefaultAll();
+        }
+
+    }
+
+    /**
+     *
+     */
+    private function setIsDefaultToTrueOnSomeRow(Vat $entity)
+    {
+        $repo = $this->entityManager
+            ->getRepository(Vat::class);
+
+        $someItem = $repo->findOneRow($entity->getId());
+
+        if (count($someItem) === 1) {
+            $someItem[0]->setIsDefault(TRUE);
+            $repo->setIsDefaultById($someItem[0]->getId());
+        } else
+            return;
     }
 
 }
--- src/Repository/VatRepository.php
+++ src/Repository/VatRepository.php
@@ -12,55 +12,78 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Vat[]    findAll()
  * @method Vat[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class VatRepository extends ServiceEntityRepository
+class VatRepository extends ServiceEntityRepository implements DefaultValueInterface
 {
+
+    private const TABLE_NAME = 'vat';
+    private const TABLE_ID = 'id';
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Vat::class);
     }
 
-    /** Set is_default to FALSE for each rows */
+    /**
+     * Return count of rows
+     * @return mixed
+     */
+    public function getCount()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('count(v.id)')
+            ->from(Vat::class, 'v');
+
+        return $qb->getQuery()->getSingleScalarResult();
+    }
+
+
+    /**
+     * Return default entry or null
+     * @return Vat|null Description
+     */
+    public function getDefaultEntity()
+    {
+        return $this->findOneBy(array('isDefault' => true));
+    }
+
+    /**
+     * Set is_default to FALSE for each rows
+     */
     public function unsetDefaultAll(): bool
     {
-
-        $conn = $this->getEntityManager()->getConnection();
-
-        $sql = "UPDATE vat SET is_default = :param";
-
-        $stmt = $conn->prepare($sql);
-        $stmt->execute(['param' => false]);
-
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->update(Vat::class, 'v')
+            ->set('v.isDefault', '?0')
+            ->getQuery()
+            ->setParameter(0, FALSE)
+            ->execute();
         return true;
-        //$qb = $this->createQueryBuilder
-
     }
 
-    // /**
-    //  * @return Vat[] Returns an array of Vat objects
-    //  */
-    /*
-    public function findByExampleField($value)
+
+    /**
+     * Return one row of table where ID is not ID in parameter
+     * @param int $id of deleting row
+     * @return mixed
+     */
+    public function findOneRow(int $id)
     {
-        return $this->createQueryBuilder('v')
-            ->andWhere('v.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('v.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $qb = $this->createQueryBuilder('v')
+            ->where('v.id != :id')
+            ->setMaxResults(1)
+            ->setParameter('id', $id);
 
-    /*
-    public function findOneBySomeField($value): ?Vat
-    {
-        return $this->createQueryBuilder('v')
-            ->andWhere('v.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $qb->getQuery()->execute();
     }
-    */
+
+    /**
+     * Set is_default = true by row ID
+     * @param int $id
+     */
+    public function setIsDefaultById(int $id)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $conn->update(self::TABLE_NAME, ['is_default' => TRUE], ['id' => $id]);
+    }
+
 }
