0cc716f851609d342725986a5e61dd379f15eeaf
patykmar <patyk.m@gmail.com>
2021-04-21 12:17:51 +0200

    PaymentTypeRepository.php implements DefaultValueInterface.php and implement functions according interface

--- src/Repository/PaymentTypeRepository.php
+++ src/Repository/PaymentTypeRepository.php
@@ -12,61 +12,83 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method PaymentType[]    findAll()
  * @method PaymentType[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class PaymentTypeRepository extends ServiceEntityRepository
+class PaymentTypeRepository extends ServiceEntityRepository implements DefaultValueInterface
 {
+
+    private const TABLE_ID = 'id';
+
+    private const TABLE_NAME = 'payment_type';
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, PaymentType::class);
     }
 
-    public function getDefaultType()
+
+    /**
+     * Return count of rows
+     * @return mixed
+     */
+    public function getCount()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('count(:id)')
+            ->from(':tableName')
+            ->setParameters([
+                'id' => self::TABLE_ID,
+                'tableName' => self::TABLE_NAME,
+            ]);
+
+        return $qb->getQuery()->getSingleScalarResult();
+    }
+
+    /**
+     * Return default entry or null
+     * @return PaymentType|null
+     */
+    public function getDefaultEntity()
     {
         return $this->findOneBy(array('isDefault' => true));
     }
 
-    /** Set is_default to FALSE for each rows */
+    /** Set is_default to FALSE for each rows
+     * @throws \Doctrine\DBAL\Driver\Exception
+     */
     public function unsetDefaultAll(): bool
     {
-
         $conn = $this->getEntityManager()->getConnection();
 
-        $sql = "UPDATE payment_type SET is_default = :param";
+        $sql = "UPDATE :table SET is_default = :isDefualt";
 
         $stmt = $conn->prepare($sql);
-        $stmt->execute(['param' => false]);
+        $stmt->execute([
+            'table' => self::TABLE_NAME,
+            'isDefualt' => false,
+        ]);
 
         return true;
-        //$qb = $this->createQueryBuilder
-
     }
 
-    // /**
-    //  * @return PaymentType[] Returns an array of PaymentType objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return mixed
+     */
+    public function findOneRow(int $id)
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $qb = $this->createQueryBuilder('pt')
+            ->where('pt.id != :id')
+            ->setMaxResults(1)
+            ->setParameter('id', $id);
 
-    /*
-    public function findOneBySomeField($value): ?PaymentType
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $qb->getQuery()->execute();
     }
-    */
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function setIsDefaultById(int $id)
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $conn->update(self::TABLE_NAME, ['is_default' => TRUE], ['id' => $id]);
+    }
 }
-
