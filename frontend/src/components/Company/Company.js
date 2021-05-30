eb188418a69f971bf09fa964ef950d823ed7216a
Martin Patyk <patyk.m@gmail.com>
2021-05-30 23:12:10 +0200

    added axios get data, show simple form

--- frontend/src/components/Company/Company.js
+++ frontend/src/components/Company/Company.js
@@ -1,10 +1,34 @@
 import React from "react";
+import axios from "axios";
+import SimpleForm from "../SimpleForm/SimpleForm";
+
+
 
 class Company extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            companies: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:8000/api/companies`)
+            .then(res => {
+                this.setState({companies: res.data});
+                console.log("Company - Axios get data");
+                console.log(this.state.companies);
+            })
+    }
+
     render() {
         return (
-            <h1>Company</h1>
+            <>
+                <h1>Company</h1>
+                <SimpleForm/>
+
+            </>
         );
     }
 }
