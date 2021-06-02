95d7747c7f5183857a8633812445601b5e6154da
patykmar <patyk.m@gmail.com>
2021-06-02 14:53:31 +0200

    Added references for Tariffs

--- src/DataFixtures/TariffFixture.php
+++ src/DataFixtures/TariffFixture.php
@@ -13,6 +13,9 @@ use Doctrine\Persistence\ObjectManager;
 class TariffFixture extends Fixture implements DependentFixtureInterface
 {
 
+    public const CZK_600 = 'czk-600';
+    public const CZK_450 = 'czk-450';
+
     /**
      * @return array
      */
@@ -36,10 +39,17 @@ class TariffFixture extends Fixture implements DependentFixtureInterface
         $tariff600->setPrice(600)
             ->setName('600 CZK per hour')
             ->setVat($vatZero);
+        $this->setReference(self::CZK_600, $tariff600);
 
-        $tariff600 = new Tariff();
-        $tariff600->setPrice(450)
+        $tariff450 = new Tariff();
+        $tariff450->setPrice(450)
             ->setName('450 CZK per hour')
             ->setVat($vatZero);
+        $this->setReference(self::CZK_450, $tariff450);
+
+        $manager->persist($tariff600);
+        $manager->persist($tariff450);
+
+        $manager->flush();
     }
 }
