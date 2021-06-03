899ff1304e49f31ed28551d7775987e5d77fc90c
patykmar <patyk.m@gmail.com>
2021-06-03 09:58:53 +0200

    added missing value duration when work_end property is know

--- src/EventListener/WorkInventorySubscriber.php
+++ src/EventListener/WorkInventorySubscriber.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 
 
 use App\Entity\WorkInventory;
+use App\Services\WorkInventoryServices;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
@@ -12,10 +13,22 @@ use Doctrine\Persistence\Event\LifecycleEventArgs;
 class WorkInventorySubscriber implements EventSubscriber
 {
 
+    private WorkInventoryServices $workInventoryServices;
+
+    /**
+     * WorkInventorySubscriber constructor.
+     * @param WorkInventoryServices $workInventoryServices
+     */
+    public function __construct(WorkInventoryServices $workInventoryServices)
+    {
+        $this->workInventoryServices = $workInventoryServices;
+    }
+
+
     /**
      * @return string[]
      */
-    public function getSubscribedEvents()
+    public function getSubscribedEvents(): array
     {
         return [
             Events::prePersist,
@@ -26,19 +39,31 @@ class WorkInventorySubscriber implements EventSubscriber
     public function prePersist(LifecycleEventArgs $args): void
     {
         if($args->getObject() instanceof WorkInventory){
-            $this->calculateDurations($args->getObject());
+            $this->calculateDuration($args->getObject());
         }
     }
 
     public function preUpdate(LifecycleEventArgs $args): void
     {
         if($args->getObject() instanceof WorkInventory){
-            $this->calculateDurations($args->getObject());
+            $this->calculateDuration($args->getObject());
         }
     }
 
-    private function calculateDurations()
+    private function calculateDuration(WorkInventory $workInventory)
     {
-        //TODO: Calculate Work duration, when you know work_end time
+        // If I know, when work ended I can calculate duration
+        if (!is_null($workInventory->getWorkEnd())) {
+            $workStart = $workInventory->getWorkStart();
+            $workEnd = $workInventory->getWorkEnd();
+
+            $workInventory->setWorkDuration(
+                $this->workInventoryServices->calculateDuration(
+                    $workStart,
+                    $workEnd
+                )
+            );
+        }
     }
+
 }
