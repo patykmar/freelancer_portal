c533e5bc28250b29ab905f9d1e1dc33314dddb1a
patykmar <patyk.m@gmail.com>
2021-06-08 13:10:59 +0200

    updated Controller for detail and edit view

--- src/Controller/Admin/InvoiceCrudController.php
+++ src/Controller/Admin/InvoiceCrudController.php
@@ -3,8 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Invoice;
+use App\Form\InvoiceItemEditFormType;
 use App\Form\InvoiceItemFormType;
-use ContainerSabFytd\getAnnotations_CacheWarmerService;
+use App\Services\InvoiceServices;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -14,55 +15,95 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Doctrine\ORM\NonUniqueResultException;
 
 class InvoiceCrudController extends AbstractCrudController
 {
+
+    private InvoiceServices $invoiceServices;
+
+    /**
+     * InvoiceCrudController constructor.
+     * @param InvoiceServices $invoiceServices
+     */
+    public function __construct(InvoiceServices $invoiceServices)
+    {
+        $this->invoiceServices = $invoiceServices;
+    }
+
+
     public static function getEntityFqcn(): string
     {
         return Invoice::class;
     }
 
 
+    /**
+     * @throws NonUniqueResultException
+     */
     public function configureFields(string $pageName): iterable
     {
         // prepare fields which want to show
         $returnArray = [
-            AssociationField::new('supplier', 'Dodavatel: '),
-            AssociationField::new('subscriber', 'Odběratel: '),
-            AssociationField::new('payment_type', 'Forma úhrady: '),
-
-            IntegerField::new('due', 'Splatnost: ')
-                ->setFormTypeOptions(['data' => 14])
-                ->setHelp('Počet dní splatnost faktury')
-                ->onlyOnForms(),
-
-
-            TextField::new('vs'),
-            TextField::new('ks')
-                ->setFormTypeOptions(['data' => 308]),
+            AssociationField::new('supplier', 'Dodavatel: ')
+                ->setRequired(true),
+            AssociationField::new('subscriber', 'Odběratel: ')
+                ->setRequired(true),
+            AssociationField::new('payment_type', 'Forma úhrady: ')
+                ->setRequired(true),
         ];
+
 
         // in case I'm editing or adding item show InvoiceItems
         switch ($pageName) {
             case Crud::PAGE_NEW:
-            case Crud::PAGE_EDIT:
+                // new Invoice form
+                $returnArray[] = IntegerField::new('due', 'Splatnost: ')
+                        ->setFormTypeOptions([
+                            'data' => 14,
+                            'attr' => [
+                                'min' => 1,
+                                'max' => 99,
+                            ],
+                        ])
+                        ->setHelp('Počet dní splatnost faktury')
+                        ->onlyOnForms()
+                        ->setRequired(true);
+
+                $returnArray[] = TextField::new('vs')
+                        ->setFormTypeOptions([
+                            'data' => $this->invoiceServices->calculateInvoiceVs(),
+                        ])
+                        ->setRequired(true);
+                $returnArray[] = TextField::new('ks')
+                        ->setFormTypeOptions(['data' => '0308']);
                 $returnArray[] = CollectionField::new('invoiceItems')
-                    ->setEntryType(InvoiceItemFormType::class)
+                    ->setEntryType(InvoiceItemFormType::class);
+
+                return $returnArray;
+
+            case Crud::PAGE_EDIT:
+                $returnArray[] = IntegerField::new('due', 'Due: ')
+                    ->setRequired(true);
+                $returnArray[] = TextField::new('vs', 'Variable symbol: ')
+                    ->setRequired(true);
+                $returnArray[] = CollectionField::new('invoiceItems')
+                    ->setEntryType(InvoiceItemEditFormType::class)
                     //TODO: nefunguje nacteni sablony
                     ->setTemplatePath('admin/invoice/invoiceItemForm.html.twig');
                 return $returnArray;
-                break;
+
             case Crud::PAGE_DETAIL:
-                $returnArray[] = DateTimeField::new('due_date');
+//                $returnArray[] = AssociationField::new('user', 'Created by: ');
+                $returnArray[] = DateTimeField::new('invoice_created');
+                $returnArray[] = IntegerField::new('due', 'Due: ');
+                $returnArray[] = DateTimeField::new('due_date','Due date: ');
                 $returnArray[] = CollectionField::new('invoiceItems')
-                        ->setEntryType(InvoiceItemFormType::class)
-                        ->setTemplatePath('admin/invoice/detail.html.twig');
-                return $returnArray;
-                break;
-            default:
+                    ->setEntryType(InvoiceItemFormType::class)
+                    ->setTemplatePath('admin/invoice/detail.html.twig');
                 return $returnArray;
         }
-
+        return $returnArray;
     }
 
     public function configureActions(Actions $actions): Actions
