8a2b1186211a41fba0dd1d3529272ae3cd6c7bc7
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-26 00:48:14 +0200

    added PasswordField and changed return values from array to generator. Create action for change password

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,11 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -18,30 +22,36 @@ class UserCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id')
-                ->onlyOnIndex(),
-            EmailField::new('email')
-                ->setRequired(true)
-                ->setHelp('Used also as login value'),
-            TextField::new('first_name')
-                ->setRequired(true),
-            TextField::new('last_name')
-                ->setRequired(true),
-            DateTimeField::new('last_login')
-                ->onlyOnIndex(),
-            DateTimeField::new('created')
-                ->onlyOnIndex()
-        ];
+        yield IdField::new('id')
+            ->onlyOnIndex();
+        yield EmailField::new('email')
+            ->setRequired(true)
+            ->setHelp('Used also as login value');
+        yield TextField::new('first_name')
+            ->setRequired(true);
+        yield TextField::new('last_name')
+            ->setRequired(true);
+        if ($pageName == Crud::PAGE_NEW) {
+            yield TextField::new('password')
+                ->setFormType(PasswordType::class);
+        }
+        yield DateTimeField::new('last_login')
+            ->onlyOnIndex();
+        yield DateTimeField::new('created')
+            ->onlyOnIndex();
     }
-    /*
-    public function configureFields(string $pageName): iterable
+
+    public function configureActions(Actions $actions): Actions
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $generatePassword = Action::new('set-new-password', '', 'fas fa-key');
+        $generatePassword
+            ->linkToRoute('user_set-new-password', function (User $user): array {
+                return ['userId' => $user->getId()];
+            });
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, $generatePassword);
+
     }
-    */
+
 }
