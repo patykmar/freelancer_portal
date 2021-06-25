d834f0ccf63451de6b8048cc7635cab271be00d7
Ing. Martin Patyk <martin@patyk.cz>
2021-06-25 15:38:11 +0200

    Update configuration of UserCrudController

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,6 +16,24 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')
+                ->onlyOnIndex(),
+            EmailField::new('email')
+                ->setRequired(true)
+                ->setHelp('Used also as login value'),
+            TextField::new('first_name')
+                ->setRequired(true),
+            TextField::new('last_name')
+                ->setRequired(true),
+            DateTimeField::new('last_login')
+                ->onlyOnIndex(),
+            DateTimeField::new('created')
+                ->onlyOnIndex()
+        ];
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
