caf9cfcbf4f2b9c8c9aa27d46d00983a161761a8
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-27 00:56:10 +0200

    load data from DB about user. Check if user exist. Try check if old password is valid

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,40 @@
 
 namespace App\Controller;
 
+use App\Controller\Admin\UserCrudController;
 use App\Form\PasswordSetFormType;
+use App\Repository\UserRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private AdminUrlGenerator $adminUrlGenerator;
+    private UserRepository $userRepository;
+    private UserPasswordEncoderInterface $encoder;
+
+    /**
+     * SecurityController constructor.
+     * @param AdminUrlGenerator $adminUrlGenerator
+     * @param UserRepository $userRepository
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(AdminUrlGenerator $adminUrlGenerator, UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
+    {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+        $this->userRepository = $userRepository;
+        $this->encoder = $encoder;
+    }
+
+
     /**
      * @Route("/login", name="app_login")
      */
@@ -42,16 +66,40 @@ class SecurityController extends AbstractController
     */
     public function setNewPassword(int $userId, Request $request): Response
     {
-        //TODO: create simple form for changing password
-        // user/admin should know his password, so three password input fields
-        // 1. old password
-        // 2. new password
-        // 3. retype new password
 
+        // generate URL to EasyAdmin controller
+        $destinationUrl = $this->adminUrlGenerator
+            ->setController(UserCrudController::class)
+            ->setAction(Crud::PAGE_INDEX);
+
+        // check if user is in DB
+        $userInDb = $this->userRepository->find($userId);
+        if (is_null($userId)) {
+            $this->addFlash('error', 'User with this ID is not in database!');
+            return $this->redirect($destinationUrl);
+        }
 
         $form = $this->createForm(PasswordSetFormType::class);
 
-        return $this->render('security/passwordSet.html.twig',[
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $formData = $form->getData();
+            if (!($this->encoder->isPasswordValid($userInDb, $formData['old-password']))) {
+                $form->addError(new FormError('The old password is not valid!'));
+            }
+
+            dump($userInDb);
+            dd($formData);
+
+            #$form->addError()
+
+
+            $this->addFlash('error', 'User with this ID is not in database!');
+            return $this->redirectToRoute('task_success');
+        }
+
+        return $this->render('security/passwordSet.html.twig', [
             'form' => $form->createView(),
             'customPageTitle' => 'Set password'
         ]);
