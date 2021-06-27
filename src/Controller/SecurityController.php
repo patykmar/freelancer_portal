4195843b5a2e56b8602cd28f3f7b1bd93a6e0c94
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-27 13:14:00 +0200

    removed encoder service, set user id in to hidden parameter of form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,32 +9,26 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     private AdminUrlGenerator $adminUrlGenerator;
     private UserRepository $userRepository;
-    private UserPasswordEncoderInterface $encoder;
 
     /**
      * SecurityController constructor.
      * @param AdminUrlGenerator $adminUrlGenerator
      * @param UserRepository $userRepository
-     * @param UserPasswordEncoderInterface $encoder
      */
-    public function __construct(AdminUrlGenerator $adminUrlGenerator, UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
+    public function __construct(AdminUrlGenerator $adminUrlGenerator, UserRepository $userRepository)
     {
         $this->adminUrlGenerator = $adminUrlGenerator;
         $this->userRepository = $userRepository;
-        $this->encoder = $encoder;
     }
-
 
     /**
      * @Route("/login", name="app_login")
@@ -80,20 +74,16 @@ class SecurityController extends AbstractController
         }
 
         $form = $this->createForm(PasswordSetFormType::class);
+        $form->get('user-id')->setData($userId);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
             $formData = $form->getData();
-            if (!($this->encoder->isPasswordValid($userInDb, $formData['old-password']))) {
-                $form->addError(new FormError('The old password is not valid!'));
-                //TODO: migrate it in to PasswordSetListener class, I can't raise form error at this time.
-            }
-
+//TODO: persist data to DB
             dump($userInDb);
             dd($formData);
 
-            #$form->addError()
 
 
             $this->addFlash('error', 'User with this ID is not in database!');
