64d0a2bcf8e407d04331ae8d0047ffb9c26bc6d2
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-27 09:15:23 +0200

    added user-id input as hidden type

--- src/Form/PasswordSetFormType.php
+++ src/Form/PasswordSetFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 
 use App\Form\EventListener\PasswordSetListener;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +16,7 @@ class PasswordSetFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('user-id', HiddenType::class)
             ->add('old-password', PasswordType::class)
             ->add('new-password', PasswordType::class)
             ->add('re-type-new-password', PasswordType::class)
