0c031a97121da0ce0f3413b25a9a308c587aae09
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-27 13:17:06 +0200

    inject necessary services and push it in to PasswordSetListener

--- src/Form/PasswordSetFormType.php
+++ src/Form/PasswordSetFormType.php
@@ -4,15 +4,37 @@
 namespace App\Form;
 
 use App\Form\EventListener\PasswordSetListener;
+use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class PasswordSetFormType extends AbstractType
 {
+
+    private UserRepository $userRepository;
+    private UserPasswordEncoderInterface $encoder;
+
+    /**
+     * PasswordSetFormType constructor.
+     * @param UserRepository $userRepository
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
+    {
+        $this->userRepository = $userRepository;
+        $this->encoder = $encoder;
+    }
+
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -21,7 +43,10 @@ class PasswordSetFormType extends AbstractType
             ->add('new-password', PasswordType::class)
             ->add('re-type-new-password', PasswordType::class)
             ->add('save', SubmitType::class)
-            ->addEventSubscriber(new PasswordSetListener());
+            ->addEventSubscriber(new PasswordSetListener(
+                $this->userRepository,
+                $this->encoder
+            ));
     }
 
 }
