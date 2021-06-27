91955a8bea53b01e678e04950d04090dde006521
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-27 13:15:50 +0200

    check if user's old password is the same as user provided and inject necessary services

--- src/Form/EventListener/PasswordSetListener.php
+++ src/Form/EventListener/PasswordSetListener.php
@@ -3,14 +3,32 @@
 
 namespace App\Form\EventListener;
 
+use App\Repository\UserRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class PasswordSetListener implements EventSubscriberInterface
 {
+
+    private UserRepository $userRepository;
+    private UserPasswordEncoderInterface $encoder;
+
+    /**
+     * PasswordSetListener constructor. Injected services from FormType class
+     * @link https://symfony.com/doc/current/form/dynamic_form_modification.html#creating-the-form-type
+     * @param UserRepository $userRepository
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $encoder)
+    {
+        $this->userRepository = $userRepository;
+        $this->encoder = $encoder;
+    }
+
 
     /**
      * @inheritDoc
@@ -23,6 +41,9 @@ class PasswordSetListener implements EventSubscriberInterface
         ];
     }
 
+    /**
+     * @param FormEvent $event
+     */
     public function onPreSubmit(FormEvent $event): void
     {
         $formUserData = $event->getData();
@@ -32,8 +53,15 @@ class PasswordSetListener implements EventSubscriberInterface
             return;
         }
 
-        if ($formUserData['new-password'] !== $formUserData['re-type-new-password']){
+        if ($formUserData['new-password'] !== $formUserData['re-type-new-password']) {
             $form->addError(new FormError('New password and re-type password is not the same!'));
         }
+
+        $userFromDb = $this->userRepository->find($formUserData['user-id']);
+
+        if (!($this->encoder->isPasswordValid($userFromDb, $formUserData['old-password']))) {
+            $form->addError(new FormError('The old password is not valid!'));
+        }
+
     }
 }
