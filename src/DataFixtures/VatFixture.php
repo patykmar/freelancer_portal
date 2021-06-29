39499a2b8b859779eff9c1f9d92b2ded588fb5b2
Ing. Martin Patyk <martin@patyk.cz>
2021-06-29 17:49:08 +0200

    Added more VAT items

--- src/DataFixtures/VatFixture.php
+++ src/DataFixtures/VatFixture.php
@@ -19,10 +19,40 @@ class VatFixture extends Fixture
             ->setPercent(0)
             ->setIsDefault(TRUE);
 
+        $vat5 = new Vat();
+        $vat5->setName("DPH 5%")
+            ->setMultiplier(105)
+            ->setPercent(5)
+            ->setIsDefault(FALSE);
+
+        $vat10 = new Vat();
+        $vat10->setName("DPH 10%")
+            ->setMultiplier(110)
+            ->setPercent(10)
+            ->setIsDefault(FALSE);
+
+        $vat15 = new Vat();
+        $vat15->setName("DPH 15%")
+            ->setMultiplier(115)
+            ->setPercent(15)
+            ->setIsDefault(FALSE);
+
+        $vat20 = new Vat();
+        $vat20->setName("DPH 20%")
+            ->setMultiplier(120)
+            ->setPercent(20)
+            ->setIsDefault(FALSE);
+
         $vat21 = new Vat();
         $vat21->setName("DPH 21%")
             ->setMultiplier(121)
             ->setPercent(21)
+            ->setIsDefault(FALSE);
+
+        $vat22 = new Vat();
+        $vat22->setName("DPH 22%")
+            ->setMultiplier(122)
+            ->setPercent(22)
             ->setIsDefault(FALSE);
 
         $this->addReference(self::NO_VAT, $vatNo);
