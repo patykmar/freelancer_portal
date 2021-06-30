7928ab0ca49f66097e10a9d2a991d48dadd3a004
Ing. Martin Patyk <patyk.m@gmail.com>
2021-06-30 23:21:46 +0200

    added missing added new VAT to manager

--- src/DataFixtures/VatFixture.php
+++ src/DataFixtures/VatFixture.php
@@ -18,48 +18,51 @@ class VatFixture extends Fixture
             ->setMultiplier(100)
             ->setPercent(0)
             ->setIsDefault(TRUE);
+        $manager->persist($vatNo);
+        $this->addReference(self::NO_VAT, $vatNo);
 
         $vat5 = new Vat();
         $vat5->setName("DPH 5%")
             ->setMultiplier(105)
             ->setPercent(5)
             ->setIsDefault(FALSE);
+        $manager->persist($vat5);
 
         $vat10 = new Vat();
         $vat10->setName("DPH 10%")
             ->setMultiplier(110)
             ->setPercent(10)
             ->setIsDefault(FALSE);
+        $manager->persist($vat10);
 
         $vat15 = new Vat();
         $vat15->setName("DPH 15%")
             ->setMultiplier(115)
             ->setPercent(15)
             ->setIsDefault(FALSE);
+        $manager->persist($vat15);
 
         $vat20 = new Vat();
         $vat20->setName("DPH 20%")
             ->setMultiplier(120)
             ->setPercent(20)
             ->setIsDefault(FALSE);
+        $manager->persist($vat20);
 
         $vat21 = new Vat();
         $vat21->setName("DPH 21%")
             ->setMultiplier(121)
             ->setPercent(21)
             ->setIsDefault(FALSE);
+        $manager->persist($vat21);
+        $this->addReference(self::VAT_21, $vat21);
 
         $vat22 = new Vat();
         $vat22->setName("DPH 22%")
             ->setMultiplier(122)
             ->setPercent(22)
             ->setIsDefault(FALSE);
-
-        $this->addReference(self::NO_VAT, $vatNo);
-        $this->addReference(self::VAT_21, $vat21);
-
-        $manager->persist($vatNo);
-        $manager->persist($vat21);
+        $manager->persist($vat22);
 
         $manager->flush();
     }
