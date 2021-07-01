8df2137f03d58439b63d2c3eb5a322197285d93b
Ing. Martin Patyk <patyk.m@gmail.com>
2021-07-01 14:32:14 +0200

    removed function which can generate invoice with work inventory

--- src/Controller/Admin/CompanyCrudController.php
+++ src/Controller/Admin/CompanyCrudController.php
@@ -8,7 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CompanyCrudController extends AbstractCrudController
@@ -18,43 +18,31 @@ class CompanyCrudController extends AbstractCrudController
         return Company::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
-            yield TextField::new('name', 'Company name: ');
-            yield TextField::new('description', 'Company description: ');
-            yield TextField::new('company_id', 'Company ID: ')
-                ->hideOnIndex();
-            yield TextField::new('vat_number', 'VAT ID: ')
-                ->hideOnIndex();
-            yield TextField::new('street', 'Address: ')
-                ->hideOnIndex();
-            yield TextField::new('city', 'City: ');
-            yield TextField::new('zip_code', 'ZIP code: ')
-                ->hideOnIndex();
-            yield AssociationField::new('country', 'County: ');
-            yield TextField::new('account_number', 'Bank account: ')
-                ->hideOnIndex();
-            yield TextField::new('iban', 'IBAN: ')
-                ->hideOnIndex();
-            yield CollectionField::new('workInventories');
-//                ->onlyOnDetail();
+        yield IdField::new('id', 'ID')
+            ->onlyOnIndex();
+        yield TextField::new('name', 'Company name: ');
+        yield TextField::new('description', 'Company description: ');
+        yield TextField::new('company_id', 'Company ID: ')
+            ->hideOnIndex();
+        yield TextField::new('vat_number', 'VAT ID: ')
+            ->hideOnIndex();
+        yield TextField::new('street', 'Address: ')
+            ->hideOnIndex();
+        yield TextField::new('city', 'City: ');
+        yield TextField::new('zip_code', 'ZIP code: ')
+            ->hideOnIndex();
+        yield AssociationField::new('country', 'County: ');
+        yield TextField::new('account_number', 'Bank account: ')
+            ->hideOnIndex();
+        yield TextField::new('iban', 'IBAN: ')
+            ->hideOnIndex();
     }
 
     public function configureActions(Actions $actions): Actions
     {
-        // if the method is not defined in a CRUD controller, link to its route
-        $sendInvoice = Action::new('workItemsByCompany', 'Gen. invoice', 'fas fa-file-invoice')
-            // 2) using a callable (useful if parameters depend on the entity instance)
-            // (the type-hint of the function argument is optional but useful)
-            ->linkToRoute('work_inventory_generate_invoice_by_company', function (Company $company): array {
-                return [
-                    'companyId' => $company->getId(),
-                ];
-            });
-
         return $actions
-            ->add(Crud::PAGE_INDEX, $sendInvoice)
             ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 
