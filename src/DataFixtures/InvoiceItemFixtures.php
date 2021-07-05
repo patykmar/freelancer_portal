fbc0652fe20562f5802c58f9481292a34228c982
Ing. Martin Patyk <patyk.m@gmail.com>
2021-07-05 02:26:18 +0200

    generate fixtures with loop and load random data

--- src/DataFixtures/InvoiceItemFixtures.php
+++ src/DataFixtures/InvoiceItemFixtures.php
@@ -4,7 +4,6 @@
 namespace App\DataFixtures;
 
 
-use App\Entity\Invoice;
 use App\Entity\InvoiceItem;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -29,229 +28,49 @@ class InvoiceItemFixtures extends Fixture implements DependentFixtureInterface
      */
     public function load(ObjectManager $manager): void
     {
-        /** @var Invoice $invoice01 */
-        $invoice01 = $this->getReference(InvoiceFixtures::INVOICE_01);
-        /** @var Invoice $invoice02 */
-        $invoice02 = $this->getReference(InvoiceFixtures::INVOICE_02);
-        /** @var Invoice $invoice03 */
-        $invoice03 = $this->getReference(InvoiceFixtures::INVOICE_03);
-        /** @var Invoice $invoice04 */
-        $invoice04 = $this->getReference(InvoiceFixtures::INVOICE_04);
-        /** @var Invoice $invoice05 */
-        $invoice05 = $this->getReference(InvoiceFixtures::INVOICE_05);
+        $invoices = [
+            $this->getReference(InvoiceFixtures::INVOICE_01),
+            $this->getReference(InvoiceFixtures::INVOICE_02),
+            $this->getReference(InvoiceFixtures::INVOICE_03),
+            $this->getReference(InvoiceFixtures::INVOICE_04),
+            $this->getReference(InvoiceFixtures::INVOICE_05)
+        ];
 
-        $vat0 = $this->getReference(VatFixture::NO_VAT);
-        $vat21 = $this->getReference(VatFixture::VAT_21);
+        $vats = [
+            $this->getReference(VatFixture::NO_VAT),
+            $this->getReference(VatFixture::VAT_21)
+        ];
 
-        $invoiceItem11 = new InvoiceItem();
-        $invoiceItem11
-            ->setInvoice($invoice01)
-            ->setVat($vat0)
-            ->setName('Lorem ipsum dolor sit amet consectetuer')
-            ->setPrice(1000000)
-            ->setUnitCount(150)
-            ->setDiscount(0)
-            ->setMargin(20);
+        $sentences = [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Nam at congue nulla.',
+            'Sed a sodales leo, fermentum lobortis tellus.', 'Morbi et risus sed enim consequat auctor id convallis massa.',
+            'Duis lacus enim, accumsan eget tristique sed, viverra et augue.', 'Curabitur ut sodales ante.',
+            'Sed vestibulum, erat eget tincidunt egestas, sem metus eleifend erat, vel suscipit est ante id arcu.',
+            'Nunc laoreet, quam sit amet pretium ultricies, justo nisi placerat libero, a molestie dolor odio vel nisl.',
+            'In hac habitasse platea dictumst.', 'Suspendisse massa arcu, laoreet eget cursus non, vulputate eget diam.',
+            'Vestibulum nisl sapien, pellentesque non consectetur et, finibus eget elit.',
+            'Phasellus quis tincidunt arcu.', 'Suspendisse sed lectus dui.', 'In laoreet libero orci, sed tincidunt sapien viverra eget.',
+            'Nam erat mauris, hendrerit sed varius id, aliquet id arcu.',
+            'Quisque elementum, nulla id sollicitudin volutpat, metus nunc pretium nisi, in varius massa lectus vitae sapien.',
+            'Nulla facilisi.', 'Suspendisse ac nisi laoreet, vulputate augue eget, porttitor arcu.',
+            'Etiam vel urna ullamcorper, dignissim urna non, consequat ex.', 'Duis tempor nibh sapien.',
+            'Donec pellentesque arcu quis feugiat dapibus.', 'Sed consequat enim et neque iaculis elementum.',
+            'Nulla massa metus, vestibulum ut eros a, feugiat vehicula tortor.', 'Pellentesque tristique molestie tortor posuere rutrum.',
+            'Praesent porttitor magna id ligula congue, a scelerisque ligula fringilla.', 'Proin non suscipit sapien.'
+        ];
 
-        $invoiceItem12 = new InvoiceItem();
-        $invoiceItem12
-            ->setInvoice($invoice01)
-            ->setVat($vat0)
-            ->setName('Sed enim auctor pede pellentesque')
-            ->setPrice(156987)
-            ->setUnitCount(1000)
-            ->setDiscount(0)
-            ->setMargin(20);
 
-        $invoiceItem13 = new InvoiceItem();
-        $invoiceItem13
-            ->setInvoice($invoice01)
-            ->setVat($vat0)
-            ->setName('Justo nascetur faucibus pellentesque')
-            ->setPrice(158648321)
-            ->setUnitCount(300)
-            ->setDiscount(10)
-            ->setMargin(20);
-
-        $invoiceItem14 = new InvoiceItem();
-        $invoiceItem14
-            ->setInvoice($invoice01)
-            ->setVat($vat21)
-            ->setName('Adipiscing hendrerit justo Ut montes sodales nisl')
-            ->setPrice(147850)
-            ->setUnitCount(3000)
-            ->setDiscount(90)
-            ->setMargin(20);
-
-        $invoiceItem21 = new InvoiceItem();
-        $invoiceItem21
-            ->setInvoice($invoice02)
-            ->setVat($vat21)
-            ->setName('Montes quis id tortor egestas enim')
-            ->setPrice(258741)
-            ->setUnitCount(300)
-            ->setDiscount(0)
-            ->setMargin(0);
-
-        $invoiceItem22 = new InvoiceItem();
-        $invoiceItem22
-            ->setInvoice($invoice02)
-            ->setVat($vat21)
-            ->setName('Odio eget nonummy Cras lorem')
-            ->setPrice(369852)
-            ->setUnitCount(100)
-            ->setDiscount(100)
-            ->setMargin(0);
-
-        $invoiceItem23 = new InvoiceItem();
-        $invoiceItem23
-            ->setInvoice($invoice02)
-            ->setVat($vat21)
-            ->setName('Pretium congue id tincidunt id ligula metus')
-            ->setPrice(2563214)
-            ->setUnitCount(1000)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem24 = new InvoiceItem();
-        $invoiceItem24
-            ->setInvoice($invoice02)
-            ->setVat($vat21)
-            ->setName('Pede tincidunt Maecenas pretium')
-            ->setPrice(2563214)
-            ->setUnitCount(1000)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem31 = new InvoiceItem();
-        $invoiceItem31
-            ->setInvoice($invoice03)
-            ->setVat($vat0)
-            ->setName('Cursus et at a id feugiat nibh')
-            ->setPrice(985476)
-            ->setUnitCount(200)
-            ->setDiscount(100)
-            ->setMargin(100);
-
-        $invoiceItem32 = new InvoiceItem();
-        $invoiceItem32
-            ->setInvoice($invoice03)
-            ->setVat($vat0)
-            ->setName('Vestibulum dapibus faucibus Aliquam')
-            ->setPrice(258741)
-            ->setUnitCount(1200)
-            ->setDiscount(0)
-            ->setMargin(0);
-
-        $invoiceItem33 = new InvoiceItem();
-        $invoiceItem33
-            ->setInvoice($invoice03)
-            ->setVat($vat0)
-            ->setName('Lacus dui habitasse auctor neque ut In Aliquam elit malesuada Sed')
-            ->setPrice(123698521)
-            ->setUnitCount(14700)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem34 = new InvoiceItem();
-        $invoiceItem34
-            ->setInvoice($invoice03)
-            ->setVat($vat0)
-            ->setName('Consectetuer eu nascetur.')
-            ->setPrice(258963)
-            ->setUnitCount(32100)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem35 = new InvoiceItem();
-        $invoiceItem35
-            ->setInvoice($invoice03)
-            ->setVat($vat0)
-            ->setName('Turpis Duis ipsum nec mi nonummy eget tellus elit rhoncus interdum.')
-            ->setPrice(654789321)
-            ->setUnitCount(1400)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem41 = new InvoiceItem();
-        $invoiceItem41
-            ->setInvoice($invoice04)
-            ->setVat($vat0)
-            ->setName('Gravida fringilla eget suscipit purus leo In ut lorem ac Nulla.')
-            ->setPrice(2547853)
-            ->setUnitCount(58200)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem42 = new InvoiceItem();
-        $invoiceItem42
-            ->setInvoice($invoice04)
-            ->setVat($vat0)
-            ->setName('Nascetur metus id dui et pretium et Aenean auctor nulla pellentesque.')
-            ->setPrice(258635)
-            ->setUnitCount(47800)
-            ->setDiscount(0)
-            ->setMargin(100);
-
-        $invoiceItem43 = new InvoiceItem();
-        $invoiceItem43
-            ->setInvoice($invoice04)
-            ->setVat($vat0)
-            ->setName('Nunc Quisque ut amet nec quam hac In egestas hac pulvinar.')
-            ->setPrice(456987)
-            ->setUnitCount(1200)
-            ->setDiscount(50)
-            ->setMargin(0);
-
-        $invoiceItem51 = new InvoiceItem();
-        $invoiceItem51
-            ->setInvoice($invoice05)
-            ->setVat($vat0)
-            ->setName('Risus wisi sollicitudin vitae risus pellentesque orci tellus faucibus eget.')
-            ->setPrice(147852)
-            ->setUnitCount(3200)
-            ->setDiscount(10)
-            ->setMargin(30);
-
-        $invoiceItem52 = new InvoiceItem();
-        $invoiceItem52
-            ->setInvoice($invoice05)
-            ->setVat($vat0)
-            ->setName('Ac gravida Maecenas Fusce interdum risus vel tempor Phasellus neque tellus.')
-            ->setPrice(258741)
-            ->setUnitCount(100)
-            ->setDiscount(1)
-            ->setMargin(1);
-
-        $invoiceItem53 = new InvoiceItem();
-        $invoiceItem53
-            ->setInvoice($invoice05)
-            ->setVat($vat0)
-            ->setName('Vitae velit Morbi ridiculus id ligula dui justo fringilla libero urna.')
-            ->setPrice(963258)
-            ->setUnitCount(10000)
-            ->setDiscount(100)
-            ->setMargin(100);
-
-        $manager->persist($invoiceItem11);
-        $manager->persist($invoiceItem12);
-        $manager->persist($invoiceItem13);
-        $manager->persist($invoiceItem14);
-        $manager->persist($invoiceItem21);
-        $manager->persist($invoiceItem22);
-        $manager->persist($invoiceItem23);
-        $manager->persist($invoiceItem24);
-        $manager->persist($invoiceItem31);
-        $manager->persist($invoiceItem32);
-        $manager->persist($invoiceItem33);
-        $manager->persist($invoiceItem34);
-        $manager->persist($invoiceItem35);
-        $manager->persist($invoiceItem41);
-        $manager->persist($invoiceItem42);
-        $manager->persist($invoiceItem43);
-        $manager->persist($invoiceItem51);
-        $manager->persist($invoiceItem52);
-        $manager->persist($invoiceItem53);
+        for ($i = 0; $i < rand(50, 500); $i++) {
+            $iif = new InvoiceItem();
+            $iif->setInvoice($invoices[rand(0, count($invoices) - 1)])
+                ->setVat($vats[rand(0, count($vats) - 1)])
+                ->setName($sentences[rand(0, count($sentences) - 1)])
+                ->setPrice(rand(100, 999999))
+                ->setUnitCount(rand(1, 99999) * (rand(1, 10) / 10))
+                ->setDiscount(rand(0, 100))
+                ->setMargin(rand(0, 300));
+            $manager->persist($iif);
+        }
 
         $manager->flush();
 
--- src/DataFixtures/WorkInventoryFixtures.php
+++ src/DataFixtures/WorkInventoryFixtures.php
@@ -9,6 +9,7 @@ use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use DateTime;
+use Exception;
 
 class WorkInventoryFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -27,171 +28,76 @@ class WorkInventoryFixtures extends Fixture implements DependentFixtureInterface
 
     /**
      * @inheritDoc
+     * @throws Exception
      */
     public function load(ObjectManager $manager)
     {
-        $companyInnovation = $this->getReference(CompanyFixture::INNOVATION_ADVISORS);
-        $companyCge = $this->getReference(CompanyFixture::CGE_SRO);
-        $companyMaxxnet = $this->getReference(CompanyFixture::COMPANY_MAXXNET);
+        $companies = [
+            $this->getReference(CompanyFixture::COMPANY_O2_CZ),
+            $this->getReference(CompanyFixture::COMPANY_TMOBILE_CZ),
+            $this->getReference(CompanyFixture::COMPANY_CEZ),
+            $this->getReference(CompanyFixture::COMPANY_CETIN_CZ),
+            $this->getReference(CompanyFixture::COMPANY_VODAFONE_CZ),
+        ];
 
-        $userAdmin = $this->getReference(UserFixture::USER_ADMIN_REFERENCE);
-        $userUser = $this->getReference(UserFixture::USER_USER_REFERENCE);
 
-        $tariff600 = $this->getReference(TariffFixture::CZK_600);
-        $tariff450 = $this->getReference(TariffFixture::CZK_450);
+        $users = [
+            $this->getReference(UserFixture::USER_ADMIN_REFERENCE),
+            $this->getReference(UserFixture::USER_USER_REFERENCE),
+        ];
 
-        $wif01 = new WorkInventory();
-        $wif01->setUser($userAdmin)
-            ->setCompany($companyMaxxnet)
-            ->setTariff($tariff450)
-            ->setDescription("Zmena routingu na lokalitě silo. Testováno prepojeni na zálohu.")
-            ->setWorkStart(new DateTime('2021-04-23 09:30:00'))
-            ->setWorkEnd(new DateTime('2021-04-23 10:40:00'));
-        $manager->persist($wif01);
 
-        $wif02 = new WorkInventory();
-        $wif02->setUser($userAdmin)
-            ->setCompany($companyMaxxnet)
-            ->setTariff($tariff450)
-            ->setDescription('Konzultace jak zapojit lokalitu silo tak, aby byla zajistena vysoka dostupnost sluzeb')
-            ->setWorkStart(new DateTime('2021-05-20 09:00:00'))
-            ->setWorkEnd(new DateTime('2021-05-20 16:00:00'));
-        $manager->persist($wif02);
+        $tariffs = [
+            $this->getReference(TariffFixture::CZK_299),
+            $this->getReference(TariffFixture::CZK_399),
+            $this->getReference(TariffFixture::CZK_499),
+            $this->getReference(TariffFixture::CZK_450),
+            $this->getReference(TariffFixture::CZK_600),
+            $this->getReference(TariffFixture::CZK_699),
+            $this->getReference(TariffFixture::CZK_999),
+        ];
 
-        $wif03 = new WorkInventory();
-        $wif03->setUser($userAdmin)
-            ->setCompany($companyMaxxnet)
-            ->setTariff($tariff450)
-            ->setDescription('Predstaveni navrhu na predelani lokality silo')
-            ->setWorkStart(new DateTime('2021-05-26 15:30:00'))
-            ->setWorkEnd(new DateTime('2021-05-26 18:00:00'));
-        $manager->persist($wif03);
 
-        $wif04 = new WorkInventory();
-        $wif04->setUser($userAdmin)
-            ->setCompany($companyMaxxnet)
-            ->setTariff($tariff450)
-            ->setDescription('LLD L2 diagram zapojeni lokality silo')
-            ->setWorkStart(new DateTime('2021-05-27 12:30:00'))
-            ->setWorkEnd(new DateTime('2021-05-27 14:30:00'));
-        $manager->persist($wif04);
+        $sentences = [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            'Nam at congue nulla.',
+            'Sed a sodales leo, fermentum lobortis tellus.',
+            'Morbi et risus sed enim consequat auctor id convallis massa.',
+            'Duis lacus enim, accumsan eget tristique sed, viverra et augue.',
+            'Curabitur ut sodales ante.',
+            'Sed vestibulum, erat eget tincidunt egestas, sem metus eleifend erat, vel suscipit est ante id arcu.',
+            'Nunc laoreet, quam sit amet pretium ultricies, justo nisi placerat libero, a molestie dolor odio vel nisl.',
+            'In hac habitasse platea dictumst.',
+            'Suspendisse massa arcu, laoreet eget cursus non, vulputate eget diam.',
+            'Vestibulum nisl sapien, pellentesque non consectetur et, finibus eget elit.',
+            'Phasellus quis tincidunt arcu.',
+            'Suspendisse sed lectus dui.',
+            'In laoreet libero orci, sed tincidunt sapien viverra eget.',
+            'Nam erat mauris, hendrerit sed varius id, aliquet id arcu.',
+            'Quisque elementum, nulla id sollicitudin volutpat, metus nunc pretium nisi, in varius massa lectus vitae sapien.',
+            'Nulla facilisi.',
+            'Suspendisse ac nisi laoreet, vulputate augue eget, porttitor arcu.',
+            'Etiam vel urna ullamcorper, dignissim urna non, consequat ex.',
+            'Duis tempor nibh sapien.',
+            'Donec pellentesque arcu quis feugiat dapibus.',
+            'Sed consequat enim et neque iaculis elementum.',
+            'Nulla massa metus, vestibulum ut eros a, feugiat vehicula tortor.',
+            'Pellentesque tristique molestie tortor posuere rutrum.',
+            'Praesent porttitor magna id ligula congue, a scelerisque ligula fringilla.',
+            'Proin non suscipit sapien.'
+        ];
 
-        $wif05 = new WorkInventory();
-        $wif05->setUser($userAdmin)
-            ->setCompany($companyMaxxnet)
-            ->setTariff($tariff450)
-            ->setDescription('LLD L2 vytvoreni pracovni verze navrhu zapojeni lokality silo')
-            ->setWorkStart(new DateTime('2021-05-28 09:30:00'))
-            ->setWorkEnd(new DateTime('2021-05-28 12:30:00'));
-        $manager->persist($wif05);
-
-        $wif11 = new WorkInventory();
-        $wif11->setUser($userUser)
-            ->setCompany($companyCge)
-            ->setTariff($tariff600)
-            ->setDescription('cge-energy.cz - Zracovani pripominek k webu cge-energy.cz z data 25.3.2021')
-            ->setWorkStart(new DateTime('2021-03-29 14:10:00'))
-            ->setWorkEnd(new DateTime('2021-03-29 16:50:00'));
-        $manager->persist($wif11);
-
-        $wif12 = new WorkInventory();
-        $wif12->setUser($userUser)
-            ->setCompany($companyCge)
-            ->setTariff($tariff600)
-            ->setDescription('cge-energy.cz - Zracovani zmena adresy mapy v kontaktech, uprava sirky stranky, instalace pluginu pro tvorbu grafu a naplneni grafu daty')
-            ->setWorkStart(new DateTime('2021-03-31 11:30:00'))
-            ->setWorkEnd(new DateTime('2021-03-31 13:20:00'));
-        $manager->persist($wif12);
-
-        $wif13 = new WorkInventory();
-        $wif13->setUser($userUser)
-            ->setCompany($companyCge)
-            ->setTariff($tariff600)
-            ->setDescription('cge-energy.cz - Predelani hlavni stranky, zmena loga solidsun, vytvoreni navrhu vzhledu stranek investice...')
-            ->setWorkStart(new DateTime('2021-03-31 20:00:00'))
-            ->setWorkEnd(new DateTime('2021-03-31 22:40:00'));
-        $manager->persist($wif13);
-
-        $wif14 = new WorkInventory();
-        $wif14->setUser($userUser)
-            ->setCompany($companyCge)
-            ->setTariff($tariff600)
-            ->setDescription('cpg-power.com - Aktualizace CMS drupal na verzi 7.79')
-            ->setWorkStart(new DateTime('2021-04-08 10:00:00'))
-            ->setWorkEnd(new DateTime('2021-04-08 10:20:00'));
-        $manager->persist($wif14);
-
-        $wif15 = new WorkInventory();
-        $wif15->setUser($userUser)
-            ->setCompany($companyCge)
-            ->setTariff($tariff600)
-            ->setDescription('cpg-power.com - zpracovano, uprava obsahu na webu')
-            ->setWorkStart(new DateTime('2021-04-08 11:00:00'))
-            ->setWorkEnd(new DateTime('2021-04-08 13:40:00'));
-        $manager->persist($wif15);
-
-        $wif16 = new WorkInventory();
-        $wif16->setUser($userUser)
-            ->setCompany($companyCge)
-            ->setTariff($tariff600)
-            ->setDescription('cpg-power.com - zpracovani pripominek')
-            ->setWorkStart(new DateTime('2021-04-08 20:00:00'))
-            ->setWorkEnd(new DateTime('2021-04-08 22:40:00'));
-        $manager->persist($wif16);
-
-        $wif21 = new WorkInventory();
-        $wif21->setUser($userAdmin)
-            ->setCompany($companyInnovation)
-            ->setTariff($tariff600)
-            ->setDescription('Doplnění webu - Innovation')
-            ->setWorkStart(new DateTime('2021-03-01 08:30:00'))
-            ->setWorkEnd(new DateTime('2021-03-01 10:30:00'));
-        $manager->persist($wif21);
-
-        $wif22 = new WorkInventory();
-        $wif22->setUser($userAdmin)
-            ->setCompany($companyInnovation)
-            ->setTariff($tariff600)
-            ->setDescription('Overeni dostupnosti domeny czechvintagedesign.cz')
-            ->setWorkStart(new DateTime('2021-03-03 13:00:00'))
-            ->setWorkEnd(new DateTime('2021-03-03 13:30:00'));
-        $manager->persist($wif22);
-
-        $wif23 = new WorkInventory();
-        $wif23->setUser($userAdmin)
-            ->setCompany($companyInnovation)
-            ->setTariff($tariff600)
-            ->setDescription('webhostingove sluzby a zjisteni stavu kde se nachazi fyzicke data webovych stranek')
-            ->setWorkStart(new DateTime('2021-03-05 20:20:00'))
-            ->setWorkEnd(new DateTime('2021-03-05 21:20:00'));
-        $manager->persist($wif23);
-
-        $wif24 = new WorkInventory();
-        $wif24->setUser($userAdmin)
-            ->setCompany($companyInnovation)
-            ->setTariff($tariff600)
-            ->setDescription('Zrizeni webhostingovych sluzeb: active24')
-            ->setWorkStart(new DateTime('2021-03-06 19:20:00'))
-            ->setWorkEnd(new DateTime('2021-03-06 20:20:00'));
-        $manager->persist($wif24);
-
-        $wif25 = new WorkInventory();
-        $wif25->setUser($userAdmin)
-            ->setCompany($companyInnovation)
-            ->setTariff($tariff600)
-            ->setDescription('Zaloha webovych stranek innovationadvisors.cz a cpg-power.com')
-            ->setWorkStart(new DateTime('2021-03-06 20:20:00'))
-            ->setWorkEnd(new DateTime('2021-03-06 22:00:00'));
-        $manager->persist($wif25);
-
-        $wif26 = new WorkInventory();
-        $wif26->setUser($userAdmin)
-            ->setCompany($companyInnovation)
-            ->setTariff($tariff600)
-            ->setDescription('vytvoreni noveho virtualniho serveru pro czechvintagedesign.cz a Instalace wordpress czechvintagedesign.cz')
-            ->setWorkStart(new DateTime('2021-03-06 22:20:00'))
-            ->setWorkEnd(new DateTime('2021-03-06 23:20:00'));
-        $manager->persist($wif26);
+        for ($i = 0; $i < rand(50, 500); $i++) {
+            $wif = new WorkInventory();
+            $wif->setUser($users[rand(0, count($users) - 1)])
+                ->setCompany($companies[rand(0, count($companies) - 1)])
+                ->setTariff($tariffs[rand(0, count($tariffs) - 1)])
+                ->setDescription($sentences[rand(0, count($sentences) - 1)])
+                ->setWorkStart(new DateTime('@' . rand((time() - (rand(50, 9999) * 60)), (time() + (rand(0, 999) * 60)))))
+                ->setWorkEnd(new DateTime('@' . rand((time() - (rand(50, 9999) * 60)), (time() + (rand(0, 999) * 60)))));
+            $manager->persist($wif);
+            unset($wif);
+        }
 
         $manager->flush();
     }
