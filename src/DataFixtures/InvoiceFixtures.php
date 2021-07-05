facb6b213637805db266560071e65f57ca23b8e1
Ing. Martin Patyk <patyk.m@gmail.com>
2021-07-06 01:38:42 +0200

    Added more references, added skoda company

--- src/DataFixtures/InvoiceFixtures.php
+++ src/DataFixtures/InvoiceFixtures.php
@@ -33,6 +33,17 @@ class InvoiceFixtures extends Fixture implements DependentFixtureInterface
     public const INVOICE_18 = 'invoice-18';
     public const INVOICE_19 = 'invoice-19';
     public const INVOICE_20 = 'invoice-20';
+    public const INVOICE_21 = 'invoice-21';
+    public const INVOICE_22 = 'invoice-22';
+    public const INVOICE_23 = 'invoice-23';
+    public const INVOICE_24 = 'invoice-24';
+    public const INVOICE_25 = 'invoice-25';
+    public const INVOICE_26 = 'invoice-26';
+    public const INVOICE_27 = 'invoice-27';
+    public const INVOICE_28 = 'invoice-28';
+    public const INVOICE_29 = 'invoice-29';
+    public const INVOICE_30 = 'invoice-30';
+
 
     /**
      * @param ObjectManager $manager
@@ -46,10 +57,13 @@ class InvoiceFixtures extends Fixture implements DependentFixtureInterface
             self::INVOICE_06, self::INVOICE_07, self::INVOICE_08, self::INVOICE_09, self::INVOICE_10,
             self::INVOICE_11, self::INVOICE_12, self::INVOICE_13, self::INVOICE_14, self::INVOICE_15,
             self::INVOICE_16, self::INVOICE_17, self::INVOICE_18, self::INVOICE_19, self::INVOICE_20,
+            self::INVOICE_21, self::INVOICE_22, self::INVOICE_23, self::INVOICE_24, self::INVOICE_25,
+            self::INVOICE_26, self::INVOICE_27, self::INVOICE_28, self::INVOICE_29, self::INVOICE_30,
         ];
 
         $paymentTypes = [
-            $this->getReference(PaymentTypeFixture::PT_HOTOVE), $this->getReference(PaymentTypeFixture::PT_PREVOD),
+            $this->getReference(PaymentTypeFixture::PT_HOTOVE),
+            $this->getReference(PaymentTypeFixture::PT_PREVOD),
         ];
 
         $companies = [
@@ -64,24 +78,26 @@ class InvoiceFixtures extends Fixture implements DependentFixtureInterface
             $this->getReference(CompanyFixture::INNOVATION_ADVISORS),
             $this->getReference(CompanyFixture::GAPPAY_SRO),
             $this->getReference(CompanyFixture::CGE_SRO),
+            $this->getReference(CompanyFixture::COMPANY_SKODA),
         ];
 
         $users = [
-            $this->getReference(UserFixture::USER_ADMIN_REFERENCE),
-            $this->getReference(UserFixture::USER_USER_REFERENCE),
+            $this->getReference(UserFixture::USER_ADMIN_01),
+            $this->getReference(UserFixture::USER_USER_01),
         ];
 
         for ($i = 0; $i < count($references); $i++) {
-            $invFxtr = new Invoice();
-            $invFxtr->setPaymentType($paymentTypes[rand(0, count($paymentTypes) - 1)])
+            $invoiceFixtures = new Invoice();
+            $invoiceFixtures
+                ->setPaymentType($paymentTypes[rand(0, count($paymentTypes) - 1)])
                 ->setSupplier($companies[0])
                 ->setSubscriber($companies[rand(1, count($companies) - 1)])
                 ->setDue(14)
                 ->setPaymentDay(new DateTime("+" . rand(5, 20) . " days"))
                 ->setUserCreated($users[rand(0, count($users) - 1)]);
-            $this->setReference($references[$i], $invFxtr);
-            $manager->persist($invFxtr);
-            unset($invFxtr);
+            $this->setReference($references[$i], $invoiceFixtures);
+            $manager->persist($invoiceFixtures);
+            unset($invoiceFixtures);
         }
 
         $manager->flush();
