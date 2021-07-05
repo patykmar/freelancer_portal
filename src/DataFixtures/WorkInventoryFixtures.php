4a18b2ce1c4a70d6808c0f16d9e937085e72f01c
Ing. Martin Patyk <patyk.m@gmail.com>
2021-07-06 01:40:19 +0200

    reformat $sentences

--- src/DataFixtures/WorkInventoryFixtures.php
+++ src/DataFixtures/WorkInventoryFixtures.php
@@ -30,7 +30,7 @@ class WorkInventoryFixtures extends Fixture implements DependentFixtureInterface
      * @inheritDoc
      * @throws Exception
      */
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $companies = [
             $this->getReference(CompanyFixture::COMPANY_O2_CZ),
@@ -40,12 +40,10 @@ class WorkInventoryFixtures extends Fixture implements DependentFixtureInterface
             $this->getReference(CompanyFixture::COMPANY_VODAFONE_CZ),
         ];
 
-
         $users = [
-            $this->getReference(UserFixture::USER_ADMIN_REFERENCE),
-            $this->getReference(UserFixture::USER_USER_REFERENCE),
+            $this->getReference(UserFixture::USER_ADMIN_01),
+            $this->getReference(UserFixture::USER_USER_01),
         ];
-
 
         $tariffs = [
             $this->getReference(TariffFixture::CZK_299),
@@ -57,33 +55,22 @@ class WorkInventoryFixtures extends Fixture implements DependentFixtureInterface
             $this->getReference(TariffFixture::CZK_999),
         ];
 
-
         $sentences = [
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'Nam at congue nulla.',
-            'Sed a sodales leo, fermentum lobortis tellus.',
-            'Morbi et risus sed enim consequat auctor id convallis massa.',
-            'Duis lacus enim, accumsan eget tristique sed, viverra et augue.',
-            'Curabitur ut sodales ante.',
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'Nam at congue nulla.',
+            'Sed a sodales leo, fermentum lobortis tellus.', 'Morbi et risus sed enim consequat auctor id convallis massa.',
+            'Duis lacus enim, accumsan eget tristique sed, viverra et augue.', 'Curabitur ut sodales ante.',
             'Sed vestibulum, erat eget tincidunt egestas, sem metus eleifend erat, vel suscipit est ante id arcu.',
             'Nunc laoreet, quam sit amet pretium ultricies, justo nisi placerat libero, a molestie dolor odio vel nisl.',
-            'In hac habitasse platea dictumst.',
-            'Suspendisse massa arcu, laoreet eget cursus non, vulputate eget diam.',
-            'Vestibulum nisl sapien, pellentesque non consectetur et, finibus eget elit.',
-            'Phasellus quis tincidunt arcu.',
-            'Suspendisse sed lectus dui.',
-            'In laoreet libero orci, sed tincidunt sapien viverra eget.',
+            'In hac habitasse platea dictumst.', 'Suspendisse massa arcu, laoreet eget cursus non, vulputate eget diam.',
+            'Vestibulum nisl sapien, pellentesque non consectetur et, finibus eget elit.', 'Phasellus quis tincidunt arcu.',
+            'Suspendisse sed lectus dui.', 'In laoreet libero orci, sed tincidunt sapien viverra eget.',
             'Nam erat mauris, hendrerit sed varius id, aliquet id arcu.',
             'Quisque elementum, nulla id sollicitudin volutpat, metus nunc pretium nisi, in varius massa lectus vitae sapien.',
-            'Nulla facilisi.',
-            'Suspendisse ac nisi laoreet, vulputate augue eget, porttitor arcu.',
-            'Etiam vel urna ullamcorper, dignissim urna non, consequat ex.',
-            'Duis tempor nibh sapien.',
-            'Donec pellentesque arcu quis feugiat dapibus.',
-            'Sed consequat enim et neque iaculis elementum.',
+            'Nulla facilisi.', 'Suspendisse ac nisi laoreet, vulputate augue eget, porttitor arcu.',
+            'Etiam vel urna ullamcorper, dignissim urna non, consequat ex.', 'Duis tempor nibh sapien.',
+            'Donec pellentesque arcu quis feugiat dapibus.', 'Sed consequat enim et neque iaculis elementum.',
             'Nulla massa metus, vestibulum ut eros a, feugiat vehicula tortor.',
-            'Pellentesque tristique molestie tortor posuere rutrum.',
-            'Praesent porttitor magna id ligula congue, a scelerisque ligula fringilla.',
+            'Pellentesque tristique molestie tortor posuere rutrum.', 'Praesent porttitor magna id ligula congue, a scelerisque ligula fringilla.',
             'Proin non suscipit sapien.'
         ];
 
@@ -98,7 +85,6 @@ class WorkInventoryFixtures extends Fixture implements DependentFixtureInterface
             $manager->persist($wif);
             unset($wif);
         }
-
         $manager->flush();
     }
 }
