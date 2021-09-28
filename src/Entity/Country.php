f2a3c4adcd2b46c6bc17a78616e4acbb3614db35
Ing. Martin Patyk <martin@patyk.cz>
2021-09-28 18:17:44 +0200

    added unsigned option for id

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -15,7 +15,7 @@ class Country
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", options={"unsigned":true})
      */
     private ?int $id = 0;
 
