e023f72be82b487269bc9c887dff76d99633595c
Martin Patyk <martin.patyk@telia.no>
2021-09-30 18:04:29 +0200

    specify unsigned of id

--- src/Entity/InfluencingTicket.php
+++ src/Entity/InfluencingTicket.php
@@ -13,7 +13,7 @@ class InfluencingTicket
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="integer", options={"unsigned":true})
      */
     private int $id = 0;
 
