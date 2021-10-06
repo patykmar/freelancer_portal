f05999fc07301d6548c5360179fae4effd64c71f
Martin Patyk <martin.patyk@telia.no>
2021-10-06 10:59:13 +0200

    create references, call init methods, create initUsers and initQueueUsers methods

--- src/DataFixtures/TicketFixtures.php
+++ src/DataFixtures/TicketFixtures.php
@@ -2,12 +2,116 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ticket;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 class TicketFixtures extends Fixture implements DependentFixtureInterface
 {
+    public const TICKET_01 = 'ticket-01';
+    public const TICKET_02 = 'ticket-02';
+    public const TICKET_03 = 'ticket-03';
+    public const TICKET_04 = 'ticket-04';
+    public const TICKET_05 = 'ticket-05';
+    public const TICKET_06 = 'ticket-06';
+    public const TICKET_07 = 'ticket-07';
+    public const TICKET_08 = 'ticket-08';
+    public const TICKET_09 = 'ticket-09';
+    public const TICKET_10 = 'ticket-10';
+    public const TICKET_11 = 'ticket-11';
+    public const TICKET_12 = 'ticket-12';
+    public const TICKET_13 = 'ticket-13';
+    public const TICKET_14 = 'ticket-14';
+    public const TICKET_15 = 'ticket-15';
+    public const TICKET_16 = 'ticket-16';
+    public const TICKET_17 = 'ticket-17';
+    public const TICKET_18 = 'ticket-18';
+    public const TICKET_19 = 'ticket-19';
+    public const TICKET_20 = 'ticket-20';
+    public const TICKET_21 = 'ticket-21';
+    public const TICKET_22 = 'ticket-22';
+    public const TICKET_23 = 'ticket-23';
+    public const TICKET_24 = 'ticket-24';
+    public const TICKET_25 = 'ticket-25';
+    public const TICKET_26 = 'ticket-26';
+    public const TICKET_27 = 'ticket-27';
+    public const TICKET_28 = 'ticket-28';
+    public const TICKET_29 = 'ticket-29';
+    public const TICKET_30 = 'ticket-30';
+    public const TICKET_31 = 'ticket-31';
+    public const TICKET_32 = 'ticket-32';
+    public const TICKET_33 = 'ticket-33';
+    public const TICKET_34 = 'ticket-34';
+    public const TICKET_35 = 'ticket-35';
+    public const TICKET_36 = 'ticket-36';
+    public const TICKET_37 = 'ticket-37';
+    public const TICKET_38 = 'ticket-38';
+    public const TICKET_39 = 'ticket-39';
+    public const TICKET_40 = 'ticket-40';
+    public const TICKET_41 = 'ticket-41';
+    public const TICKET_42 = 'ticket-42';
+    public const TICKET_43 = 'ticket-43';
+    public const TICKET_44 = 'ticket-44';
+    public const TICKET_45 = 'ticket-45';
+    public const TICKET_46 = 'ticket-46';
+    public const TICKET_47 = 'ticket-47';
+    public const TICKET_48 = 'ticket-48';
+    public const TICKET_49 = 'ticket-49';
+    public const TICKET_50 = 'ticket-50';
+    public const TICKET_51 = 'ticket-51';
+    public const TICKET_52 = 'ticket-52';
+    public const TICKET_53 = 'ticket-53';
+    public const TICKET_54 = 'ticket-54';
+    public const TICKET_55 = 'ticket-55';
+    public const TICKET_56 = 'ticket-56';
+    public const TICKET_57 = 'ticket-57';
+    public const TICKET_58 = 'ticket-58';
+    public const TICKET_59 = 'ticket-59';
+    public const TICKET_60 = 'ticket-60';
+    public const TICKET_61 = 'ticket-61';
+    public const TICKET_62 = 'ticket-62';
+    public const TICKET_63 = 'ticket-63';
+    public const TICKET_64 = 'ticket-64';
+    public const TICKET_65 = 'ticket-65';
+    public const TICKET_66 = 'ticket-66';
+    public const TICKET_67 = 'ticket-67';
+    public const TICKET_68 = 'ticket-68';
+    public const TICKET_69 = 'ticket-69';
+    public const TICKET_70 = 'ticket-70';
+    public const TICKET_71 = 'ticket-71';
+    public const TICKET_72 = 'ticket-72';
+    public const TICKET_73 = 'ticket-73';
+    public const TICKET_74 = 'ticket-74';
+    public const TICKET_75 = 'ticket-75';
+    public const TICKET_76 = 'ticket-76';
+    public const TICKET_77 = 'ticket-77';
+    public const TICKET_78 = 'ticket-78';
+    public const TICKET_79 = 'ticket-79';
+    public const TICKET_80 = 'ticket-80';
+    public const TICKET_81 = 'ticket-81';
+    public const TICKET_82 = 'ticket-82';
+    public const TICKET_83 = 'ticket-83';
+    public const TICKET_84 = 'ticket-84';
+    public const TICKET_85 = 'ticket-85';
+    public const TICKET_86 = 'ticket-86';
+    public const TICKET_87 = 'ticket-87';
+    public const TICKET_88 = 'ticket-88';
+    public const TICKET_89 = 'ticket-89';
+    public const TICKET_90 = 'ticket-90';
+    public const TICKET_91 = 'ticket-91';
+    public const TICKET_92 = 'ticket-92';
+    public const TICKET_93 = 'ticket-93';
+    public const TICKET_94 = 'ticket-94';
+    public const TICKET_95 = 'ticket-95';
+    public const TICKET_96 = 'ticket-96';
+    public const TICKET_97 = 'ticket-97';
+    public const TICKET_98 = 'ticket-98';
+    public const TICKET_99 = 'ticket-99';
+    public const TICKET_100 = 'ticket-100';
+
+    public array $ticket_references;
+
     private array $serviceCatalogs;
     private array $ticketTypes;
 
@@ -16,8 +120,28 @@ class TicketFixtures extends Fixture implements DependentFixtureInterface
     private array $impact;
 
     private array $generalStates;
-    private array $ticketCloseState;
     private array $ticketState;
+
+    private array $users;
+    private array $queueUsers;
+
+    private array $references = [
+        self::TICKET_01, self::TICKET_02, self::TICKET_03, self::TICKET_04, self::TICKET_05, self::TICKET_06, self::TICKET_07,
+        self::TICKET_08, self::TICKET_09, self::TICKET_10, self::TICKET_11, self::TICKET_12, self::TICKET_13, self::TICKET_14,
+        self::TICKET_15, self::TICKET_16, self::TICKET_17, self::TICKET_18, self::TICKET_19, self::TICKET_20, self::TICKET_21,
+        self::TICKET_22, self::TICKET_23, self::TICKET_24, self::TICKET_25, self::TICKET_26, self::TICKET_27, self::TICKET_28,
+        self::TICKET_29, self::TICKET_30, self::TICKET_31, self::TICKET_32, self::TICKET_33, self::TICKET_34, self::TICKET_35,
+        self::TICKET_36, self::TICKET_37, self::TICKET_38, self::TICKET_39, self::TICKET_40, self::TICKET_41, self::TICKET_42,
+        self::TICKET_43, self::TICKET_44, self::TICKET_45, self::TICKET_46, self::TICKET_47, self::TICKET_48, self::TICKET_49,
+        self::TICKET_50, self::TICKET_51, self::TICKET_52, self::TICKET_53, self::TICKET_54, self::TICKET_55, self::TICKET_56,
+        self::TICKET_57, self::TICKET_58, self::TICKET_59, self::TICKET_60, self::TICKET_61, self::TICKET_62, self::TICKET_63,
+        self::TICKET_64, self::TICKET_65, self::TICKET_66, self::TICKET_67, self::TICKET_68, self::TICKET_69, self::TICKET_70,
+        self::TICKET_71, self::TICKET_72, self::TICKET_73, self::TICKET_74, self::TICKET_75, self::TICKET_76, self::TICKET_77,
+        self::TICKET_78, self::TICKET_79, self::TICKET_80, self::TICKET_81, self::TICKET_82, self::TICKET_83, self::TICKET_84,
+        self::TICKET_85, self::TICKET_86, self::TICKET_87, self::TICKET_88, self::TICKET_89, self::TICKET_90, self::TICKET_91,
+        self::TICKET_92, self::TICKET_93, self::TICKET_94, self::TICKET_95, self::TICKET_96, self::TICKET_97, self::TICKET_98,
+        self::TICKET_99, self::TICKET_100
+    ];
 
     /**
      * @inheritDoc
@@ -28,6 +152,7 @@ class TicketFixtures extends Fixture implements DependentFixtureInterface
             ServiceCatalogFixtures::class,
             TicketTypeFixtures::class,
             InfluencingTicketFixtures::class,
+            UserFixture::class,
         ];
     }
 
@@ -36,7 +161,30 @@ class TicketFixtures extends Fixture implements DependentFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-        // TODO: Implement load() method.
+
+        $this->initGeneralSTate();
+        $this->initInfluencingTicket();
+        $this->initServiceCatalogs();
+        $this->initTicketTypes();
+        $this->initUsers();
+        $this->initQueueUsers();
+
+        for ($i = 0; $i < 100; $i++) {
+            $ticket = new Ticket();
+            $ticket->setServiceCatalog($this->serviceCatalogs[rand(0, count($this->serviceCatalogs) - 1)])
+                ->setUserCreated($this->users[rand(0, count($this->users) - 1)])
+                ->setTicketState($this->ticketState[rand(0, count($this->ticketState) - 1)])
+                ->setTicketType($this->ticketTypes[rand(0, count($this->ticketTypes) - 1)])
+                ->setPriority($this->priorities[rand(0, count($this->priorities) - 1)])
+                ->setImpact($this->impact[rand(0, count($this->impact) - 1)])
+                ->setQueueUser($this->queueUsers[rand(0, count($this->queueUsers) - 1)])
+                ->setDescriptionTitle(InvoiceItemFixtures::$sentences[rand(0, count(InvoiceItemFixtures::$sentences) - 1)])
+                ->setDescriptionBody(InvoiceItemFixtures::$sentences[rand(0, count(InvoiceItemFixtures::$sentences) - 1)]);
+            $this->addReference($this->references[$i], $ticket);
+            $manager->persist($ticket);
+            unset($ticket);
+        }
+        $manager->flush();
     }
 
     private function initServiceCatalogs(): void
@@ -67,12 +215,6 @@ class TicketFixtures extends Fixture implements DependentFixtureInterface
         );
     }
 
-    private function initPrioritiesAndImpact(): void
-    {
-        $this->priorities = $this->influencingTicketFixtures->generatorPriority();
-        $this->impact = $this->influencingTicketFixtures->generatorImpact();
-    }
-
     private function initInfluencingTicket(): void
     {
         $this->influencingTickets[] = array(
@@ -91,7 +233,7 @@ class TicketFixtures extends Fixture implements DependentFixtureInterface
                 $this->priorities[] = $influencingTicket;
 
             if ($influencingTicket['isForImpact'])
-                $this->priorities[] = $influencingTicket;
+                $this->impact[] = $influencingTicket;
         }
     }
 
@@ -114,12 +256,33 @@ class TicketFixtures extends Fixture implements DependentFixtureInterface
             $this->getReference(GeneralStateFixtures::GS_STATE_14),
         );
 
-        foreach ($this->generalStates as $generalStateItem){
-            if ($generalStateItem['isForCloseState'])
-                $this->ticketCloseState[] = $generalStateItem;
-
+        foreach ($this->generalStates as $generalStateItem) {
             if ($generalStateItem['isForTicket'])
                 $this->ticketState[] = $generalStateItem;
         }
     }
+
+    private function initUsers(): void
+    {
+        $this->users = array(
+            $this->getReference(UserFixture::USER_FIXTURE_01),
+            $this->getReference(UserFixture::USER_FIXTURE_02),
+            $this->getReference(UserFixture::USER_FIXTURE_03),
+            $this->getReference(UserFixture::USER_FIXTURE_04),
+        );
+    }
+
+    private function initQueueUsers(): void
+    {
+        $this->queueUsers = array(
+            $this->getReference(QueueUserFixtures::QU_FIXTURES_01),
+            $this->getReference(QueueUserFixtures::QU_FIXTURES_02),
+            $this->getReference(QueueUserFixtures::QU_FIXTURES_03),
+            $this->getReference(QueueUserFixtures::QU_FIXTURES_04),
+            $this->getReference(QueueUserFixtures::QU_FIXTURES_05),
+            $this->getReference(QueueUserFixtures::QU_FIXTURES_06),
+        );
+    }
+
+
 }
