f87abc86a4dfb431cf2f7414925ea4985b3c0565
Ing. Martin Patyk <patyk.m@gmail.com>
2021-10-10 01:12:11 +0200

    updated admin menu links to new crud controllers

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ci;
 use App\Entity\Company;
 use App\Entity\Country;
+use App\Entity\GeneralState;
+use App\Entity\InfluencingTicket;
 use App\Entity\Invoice;
 use App\Entity\PaymentType;
+use App\Entity\Queue;
 use App\Entity\ServiceCatalog;
 use App\Entity\Tariff;
+use App\Entity\Ticket;
+use App\Entity\TicketType;
 use App\Entity\User;
 use App\Entity\Vat;
 use App\Entity\WorkInventory;
@@ -56,21 +62,24 @@ class DashboardController extends AbstractDashboardController
 
         yield MenuItem::section("Application", 'fas fa-rocket');
         yield MenuItem::linkToCrud("Invoices", 'fas fa-file-invoice-dollar', Invoice::class);
-
+        yield MenuItem::linkToCrud("Tickets", 'far fa-calendar-check', Ticket::class);
 
         yield MenuItem::section('Work payed per hour');
         yield MenuItem::linkToCrud('Work inventory', 'fas fa-clipboard-list', WorkInventory::class);
         yield MenuItem::linkToRoute("Unpaid work", 'fab fa-creative-commons-nc-eu', 'work_inventory_unpaid');
 
-        yield MenuItem::section("Lists", 'fas fa-list-ol');
+        yield MenuItem::section("Catalogs", 'fas fa-list-ol');
+        yield MenuItem::linkToCrud('Catalog items', 'fab fa-servicestack', Ci::class);
         yield MenuItem::linkToCrud("Companies", 'fas fa-building', Company::class);
         yield MenuItem::linkToCrud("Countries", 'fas fa-globe-europe', Country::class);
+        yield MenuItem::linkToCrud('General state', 'fas fa-traffic-light', GeneralState::class);
+        yield MenuItem::linkToCrud('Influencing ticket', 'fas fa-adjust', InfluencingTicket::class);
         yield MenuItem::linkToCrud("Payment methods", 'fas fa-wallet', PaymentType::class);
-        yield MenuItem::linkToCrud('VAT', 'fas fa-hand-holding-usd', Vat::class);
-
-        yield MenuItem::section("Payment");
-        yield MenuItem::linkToCrud('Tariffs', 'fas fa-receipt', Tariff::class);
+        yield MenuItem::linkToCrud('Queue', 'fas fa-exchange-alt', Queue::class);
         yield MenuItem::linkToCrud('Service catalog', 'fas fa-warehouse', ServiceCatalog::class);
+        yield MenuItem::linkToCrud('Tariffs', 'fas fa-receipt', Tariff::class);
+        yield MenuItem::linkToCrud('Ticket type', 'far fa-arrow-alt-circle-up', TicketType::class);
+        yield MenuItem::linkToCrud('VAT', 'fas fa-hand-holding-usd', Vat::class);
 
         yield MenuItem::section('Users');
         yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
