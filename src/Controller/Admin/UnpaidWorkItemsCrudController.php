65258a8b3980c65ded963054ce7456c6b64a1a45
Ing. Martin Patyk <patyk.m@gmail.com>
2021-10-10 17:39:52 +0200

    configured fields

--- src/Controller/Admin/UnpaidWorkItemsCrudController.php
+++ src/Controller/Admin/UnpaidWorkItemsCrudController.php
@@ -3,7 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\UnpaidWorkItems;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UnpaidWorkItemsCrudController extends AbstractCrudController
 {
@@ -12,14 +19,50 @@ class UnpaidWorkItemsCrudController extends AbstractCrudController
         return UnpaidWorkItems::class;
     }
 
-    /*
+    /**
+     * @param Crud $crud
+     * @return Crud
+     */
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle(Crud::PAGE_INDEX, 'Unpaid work items');
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('companyName'),
+            TextField::new('tariffName'),
+            NumberField::new('workDurationTotal'),
+            MoneyField::new('pricePerUnit')
+                ->setCurrency('CZK'),
+            MoneyField::new('totalPrice')
+                ->setCurrency('CZK'),
         ];
     }
-    */
+
+    /**
+     * @param Actions $actions
+     * @return Actions
+     */
+    public function configureActions(Actions $actions): Actions
+    {
+        $generateInvoiceAction = Action::NEW(
+            'generateInvoice',
+            'Generate invoice',
+            'fas fa-file-invoice'
+        );
+        $generateInvoiceAction->linkToRoute(
+            'work_inventory_generate_invoice_by_company',
+            fn(UnpaidWorkItems $inventory): array => ['companyId' => $inventory->getCompanyId()]
+        );
+
+        return $actions
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->add(Crud::PAGE_INDEX, $generateInvoiceAction);
+    }
 }
