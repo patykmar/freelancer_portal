2dadcae3cbcc8df3304946a967fc426e18cf82f1
Martin Patyk <martin.patyk@telia.no>
2021-10-28 16:58:06 +0200

    fix entity name

--- src/DataFixtures/CountryFixtures.php
+++ src/DataFixtures/CountryFixtures.php
@@ -18,18 +18,18 @@ class CountryFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $countries = [
-            ['name' => 'Česká republika', 'Iso3166Alpha3' => 'CZE', 'ref' => self::COUNTRY_CZ_REFERENCE],
-            ['name' => 'Slovenská republika', 'Iso3166Alpha3' => 'SVK', 'ref' => self::COUNTRY_SK_REFERENCE],
-            ['name' => 'Polská republika', 'Iso3166Alpha3' => 'POL', 'ref' => self::COUNTRY_PL_REFERENCE],
-            ['name' => 'Republika Rakousko', 'Iso3166Alpha3' => 'AUT', 'ref' => self::COUNTRY_A_REFERENCE],
-            ['name' => 'Spolková republika Německo', 'Iso3166Alpha3' => 'DEU', 'ref' => self::COUNTRY_D_REFERENCE],
+            ['name' => 'Česká republika', 'iso3166alpha3' => 'CZE', 'ref' => self::COUNTRY_CZ_REFERENCE],
+            ['name' => 'Slovenská republika', 'iso3166alpha3' => 'SVK', 'ref' => self::COUNTRY_SK_REFERENCE],
+            ['name' => 'Polská republika', 'iso3166alpha3' => 'POL', 'ref' => self::COUNTRY_PL_REFERENCE],
+            ['name' => 'Republika Rakousko', 'iso3166alpha3' => 'AUT', 'ref' => self::COUNTRY_A_REFERENCE],
+            ['name' => 'Spolková republika Německo', 'iso3166alpha3' => 'DEU', 'ref' => self::COUNTRY_D_REFERENCE],
         ];
 
         for ($i = 0; $i < count($countries); $i++) {
             $country = new Country();
             $country
                 ->setName($countries[$i]['name'])
-                ->setIso3166Alpha3($countries[$i]['Iso3166Alpha3']);
+                ->setIso3166Alpha3($countries[$i]['iso3166alpha3']);
 
             $manager->persist($country);
             $this->addReference($countries[$i]['ref'], $country);
