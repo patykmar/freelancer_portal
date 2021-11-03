892d25655c53c92208a36cf1318669bbe9eaec47
Ing. Martin Patyk <martin@patyk.cz>
2021-11-03 21:00:07 +0100

    specify DTO classes for de(serializable)

--- src/Entity/Vat.php
+++ src/Entity/Vat.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\VatRepository;
+use App\Dto\VatDto;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -11,7 +12,10 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=VatRepository::class)
- * @ApiResource
+ * @ApiResource(
+ *     input=VatDto::class,
+ *     output=VatDto::class
+ * )
  */
 class Vat
 {
@@ -185,6 +189,4 @@ class Vat
     {
         return $this->name;
     }
-
-
 }
