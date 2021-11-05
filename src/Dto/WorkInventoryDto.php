6be541bc215a3ff09f934e0c3929e8ade9b6976c
Ing. Martin Patyk <martin@patyk.cz>
2021-11-05 22:33:52 +0100

    rewrite class, use array instead of field and filedName. Better result via API.

--- src/Dto/WorkInventoryDto.php
+++ src/Dto/WorkInventoryDto.php
@@ -6,19 +6,13 @@ use DateTimeInterface;
 
 class WorkInventoryDto
 {
-    public int $id;
-    public string $name;
+    public ?int $id;
     public string $description;
-    public string $company_id;
-    public string $vat_number;
-    public DateTimeInterface $created;
-    public DateTimeInterface $modify;
-    public string $street;
-    public string $city;
-    public string $zip_code;
-    public int $country;
-    public string $countryName;
-    public string $account_number;
-    public string $iban;
-    public bool $isSupplier;
+    public array $tariff;
+    public DateTimeInterface $work_start;
+    public ?DateTimeInterface $work_end;
+    public array $user;
+    public ?int $invoice;
+    public array $company;
+    public ?float $work_duration;
 }
