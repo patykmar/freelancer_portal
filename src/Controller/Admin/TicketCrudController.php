61a8620808b358703d650afad31385fc0f880263
Ing. Martin Patyk <patyk.m@gmail.com>
2021-11-06 22:05:42 +0100

    configure fields

--- src/Controller/Admin/TicketCrudController.php
+++ src/Controller/Admin/TicketCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Ticket;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -20,24 +21,41 @@ class TicketCrudController extends AbstractCrudController
         return Ticket::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
+        $idField = IdField::new('id');
+        // filtered only valid State, Type, Impact and Priority for Ticket entity
+        $ticketTypeField = AssociationField::new('ticketType')
+            ->setQueryBuilder(fn(QueryBuilder $queryBuilder) => $queryBuilder->andWhere('entity.isDisable=0'));
+        $ticketStateField = AssociationField::new('ticketState')
+            ->setQueryBuilder(fn(QueryBuilder $queryBuilder) => $queryBuilder->andWhere('entity.isForTicket=1'));
+        $priorityField = AssociationField::new('priority')
+            ->setQueryBuilder(fn(QueryBuilder $builder) => $builder
+                ->andWhere('entity.isForPriority = 1')
+                ->orderBy('entity.coefficientPrice'));
+        $impactField = AssociationField::new('impact')
+            ->setQueryBuilder(fn(QueryBuilder $builder) => $builder
+                ->andWhere('entity.isForImpact = 1')
+                ->orderBy('entity.coefficientPrice'));
+        $closeStateField = AssociationField::new('ticketCloseState')
+            ->setQueryBuilder(fn(QueryBuilder $builder) => $builder
+                ->andWhere('entity.isForCloseState = 1')
+                ->orderBy('entity.coefficientPrice'));
+
         switch ($pageName) {
             case Crud::PAGE_INDEX:
-                yield IdField::new('id');
+                yield $idField;
                 yield TextField::new('descriptionTitle', 'Title');
                 yield TextField::new('queueUser.user.getFullName', 'Assigned to');
-                yield TextField::new('priority', 'Priority');
-                yield TextField::new('ticketState', 'State');
+                yield $priorityField;
+                yield $ticketStateField;
                 yield DateField::new('createdDatetime', 'Created');
                 yield DateField::new('deliveryDatetime', 'Delivery');
                 break;
             case Crud::PAGE_EDIT:
-                yield IdField::new('id')
-                    ->setFormTypeOption('disabled', true);
-                yield AssociationField::new('ticketType');
-                yield AssociationField::new('ticketState');
+                yield $idField->setFormTypeOption('disabled', true);
+                yield $ticketTypeField;
+                yield $ticketStateField;
                 yield DateTimeField::new('createdDatetime', 'Created')
                     ->setFormTypeOption('disabled', true);
                 yield DateTimeField::new('reactionDatetime', 'Reaction time')
@@ -46,7 +64,6 @@ class TicketCrudController extends AbstractCrudController
                     ->setFormTypeOption('disabled', true);
                 yield DateTimeField::new('closedDatetime', 'Closed time')
                     ->setFormTypeOption('disabled', true);
-
                 yield AssociationField::new('serviceCatalog');
                 yield AssociationField::new('ci');
                 yield AssociationField::new('queueUser', 'Assigned to');
@@ -54,21 +71,34 @@ class TicketCrudController extends AbstractCrudController
                     ->setFormTypeOption('disabled', true);
                 yield AssociationField::new('userResolved', 'Resolved by')
                     ->setFormTypeOption('disabled', true);
-                yield AssociationField::new('priority');
-                yield AssociationField::new('impact');
+                yield $priorityField;
+                yield $impactField;
                 yield TextField::new('descriptionTitle');
                 yield TextEditorField::new('descriptionBody');
-                yield AssociationField::new('ticketCloseState');
+                yield $closeStateField;
+                yield TextEditorField::new('closedNotes');
+                yield CollectionField::new('logs')
+                    ->setFormTypeOption('disabled', true)
+//                    ->setCustomOption('compound',true)
+                    ->allowAdd(false)
+                    ->allowDelete(false)
+//                    ->addHtmlContentsToBody("<br />")
+//                    ->addHtmlContentsToBody('<b> ... </b>')
+                ;
+                break;
+            case Crud::PAGE_NEW:
+                yield AssociationField::new('ticketType');
+                yield AssociationField::new('ci');
+                yield AssociationField::new('serviceCatalog');
+                yield $priorityField;
+                yield $impactField;
+                yield TextField::new('descriptionTitle');
+                yield TextEditorField::new('descriptionBody');
+                yield $closeStateField;
                 yield TextEditorField::new('closedNotes');
                 yield CollectionField::new('logs')
                     ->setFormTypeOption('disabled', true);
                 break;
         }
-//        return [
-//
-//            ,
-//            TextEditorField::new('description'),
-//        ];
     }
-
 }
