a2c83a4225a788848fe9e9733541dba0ac7bb6f8
Martin Patyk <martin.patyk@telia.no>
2021-11-13 13:06:35 +0100

    created InvoiceItemDto

---
+++ src/Dto/InvoiceItemDto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Dto;
+
+class InvoiceItemDto
+{
+    public int $id;
+    public int $invoice;
+    public array $vat;
+    public string $name;
+    public int $price;
+    public int $margin;
+    public int $margin_total;
+    public int $price_inc_margin;
+    public int $discount;
+    public int $discount_total;
+    public int $price_inc_margin_minus_discount;
+    public int $price_inc_margin_discount_multi_vat;
+    public int $price_inc_margin_multi_vat;
+    public float $unit_count;
+    public int $total_price_inc_margin_discount_vat;
+    public int $total_price_inc_margin_vat;
+}
