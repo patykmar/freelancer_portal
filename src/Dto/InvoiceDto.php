6a335c1d5ebe825d7fa618af144c3208daf0edef
Martin Patyk <martin.patyk@telia.no>
2021-11-13 13:06:20 +0100

    created InvoiceDto

---
+++ src/Dto/InvoiceDto.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Dto;
+
+use DateTimeInterface;
+
+class InvoiceDto
+{
+    public ?int $id;
+    public array $supplier;
+    public array $subscriber;
+    public array $paymentType;
+    public int $due;
+    public DateTimeInterface $invoiceCreated;
+    public DateTimeInterface $dueDate;
+    public ?DateTimeInterface $paymentDate;
+    public array $userCreated;
+    public ?string $vs;
+    public ?string $ks;
+    public array $invoiceItems;
+    public array $workInventories;
+    public ?string $name;
+
+}
