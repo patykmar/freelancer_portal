33926b48e8fee1ff469e30e8b966d7670fd681a1
Martin Patyk <martin.patyk@telia.no>
2021-11-13 13:07:17 +0100

    created InvoiceOutputDataTransformer

---
+++ src/DataTransformer/InvoiceOutputDataTransformer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataTransformer;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\InvoiceDto;
+use App\Dto\Mapper\InvoiceMapper;
+use App\Entity\Invoice;
+
+class InvoiceOutputDataTransformer implements DataTransformerInterface
+{
+    private InvoiceMapper $invoiceMapper;
+
+    /**
+     * @param InvoiceMapper $invoiceMapper
+     */
+    public function __construct(InvoiceMapper $invoiceMapper)
+    {
+        $this->invoiceMapper = $invoiceMapper;
+    }
+
+    /**
+     * @param object $object
+     * @param string $to
+     * @param array $context
+     * @return InvoiceDto|object
+     */
+    public function transform($object, string $to, array $context = []): InvoiceDto
+    {
+        return $this->invoiceMapper->toDto($object);
+    }
+
+    /**
+     * @param array|object $data
+     * @param string $to
+     * @param array $context
+     * @return bool
+     */
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        return InvoiceDto::class === $to && $data instanceof Invoice;
+    }
+}
