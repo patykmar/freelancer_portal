d5b2ac27c62514df8dee662063aab4c4098d1f96
Ing. Martin Patyk <martin@patyk.cz>
2021-11-13 19:25:55 +0100

    implemented toEntity() method

--- src/Dto/Mapper/CompanyMapper.php
+++ src/Dto/Mapper/CompanyMapper.php
@@ -2,38 +2,66 @@
 
 namespace App\Dto\Mapper;
 
-use App\Dto\CompanyDto;
+use App\Dto\CompanyDtoOut;
+use App\Dto\CompanyDtoIn;
 use App\Entity\Company;
+use App\Repository\CountryRepository;
 
 class CompanyMapper implements MapperInterface
 {
+    private CountryRepository $countryRepository;
+
+    /**
+     * @param CountryRepository $countryRepository
+     */
+    public function __construct(CountryRepository $countryRepository)
+    {
+        $this->countryRepository = $countryRepository;
+    }
+
+    /**
+     * @param CompanyDtoIn|object $dto
+     * @return Company|object
+     */
     public function toEntity($dto)
     {
-        // TODO: Implement toEntity() method.
+        $companyEntity = new Company();
+        $companyEntity->setName($dto->name);
+        $companyEntity->setDescription($dto->description);
+        $companyEntity->setCompanyId($dto->companyId);
+        $companyEntity->setVatNumber($dto->vatNumber);
+        $companyEntity->setStreet($dto->street);
+        $companyEntity->setCity($dto->city);
+        $companyEntity->setZipCode($dto->zipCode);
+        $companyEntity->setCountry($this->countryRepository->find($dto->country));
+        $companyEntity->setAccountNumber($dto->accountNumber);
+        $companyEntity->setIban($dto->iban);
+        $companyEntity->setIsSupplier($dto->isSupplier);
+        return $companyEntity;
     }
 
     /**
      * @param Company|object $entity
-     * @return CompanyDto
+     * @return CompanyDtoOut
      */
-    public function toDto(object $entity): CompanyDto
+    public function toDto(object $entity): CompanyDtoOut
     {
-        $companyDto = new CompanyDto();
-        $companyDto->id = $entity->getId();
-        $companyDto->name = $entity->getName();
-        $companyDto->description = $entity->getDescription();
-        $companyDto->companyId = $entity->getCompanyId();
-        $companyDto->vatNumber = $entity->getVatNumber();
-        $companyDto->created = $entity->getCreated();
-        $companyDto->modify = $entity->getModify();
-        $companyDto->street = $entity->getStreet();
-        $companyDto->city = $entity->getCity();
-        $companyDto->zipCode = $entity->getZipCode();
-        $companyDto->country['id'] = $entity->getCountry()->getId();
-        $companyDto->country['name'] = $entity->getCountry()->getName();
-        $companyDto->accountNumber = $entity->getAccountNumber();
-        $companyDto->iban = $entity->getIban();
-        $companyDto->isSupplier = $entity->getIsSupplier();
-        return $companyDto;
+        $companyDtoOut = new CompanyDtoOut();
+        $companyDtoOut->id = $entity->getId();
+        $companyDtoOut->name = $entity->getName();
+        $companyDtoOut->description = $entity->getDescription();
+        $companyDtoOut->companyId = $entity->getCompanyId();
+        $companyDtoOut->vatNumber = $entity->getVatNumber();
+        $companyDtoOut->created = $entity->getCreated();
+        $companyDtoOut->modify = $entity->getModify();
+        $companyDtoOut->street = $entity->getStreet();
+        $companyDtoOut->city = $entity->getCity();
+        $companyDtoOut->zipCode = $entity->getZipCode();
+        $companyDtoOut->country['id'] = $entity->getCountry()->getId();
+        $companyDtoOut->country['name'] = $entity->getCountry()->getName();
+        $companyDtoOut->accountNumber = $entity->getAccountNumber();
+        $companyDtoOut->iban = $entity->getIban();
+        $companyDtoOut->isSupplier = $entity->getIsSupplier();
+        return $companyDtoOut;
     }
 }
