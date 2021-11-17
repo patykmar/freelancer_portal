8232c732953d6002f84d0178cddb1a3f4c6d6956
Ing. Martin Patyk <patyk.m@gmail.com>
2021-11-17 22:20:29 +0100

    created class InvoiceInputDataTransformer

---
+++ src/DataTransformer/Input/InvoiceInputDataTransformer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DataTransformer\Input;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\Mapper\InvoiceMapper;
+use App\Entity\Invoice;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
+class InvoiceInputDataTransformer implements DataTransformerInterface
+{
+    private InvoiceMapper $invoiceMapper;
+
+    /**
+     * @param InvoiceMapper $invoiceMapper
+     */
+    public function __construct(InvoiceMapper $invoiceMapper)
+    {
+        $this->invoiceMapper = $invoiceMapper;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function transform($object, string $to, array $context = [])
+    {
+        if (isset($context[AbstractNormalizer::OBJECT_TO_POPULATE])) {
+            // PUT method
+            return $this->invoiceMapper->fullFillEntity(
+                $context[AbstractNormalizer::OBJECT_TO_POPULATE],
+                $object
+            );
+        } else {
+            // POST method
+            return $this->invoiceMapper->toEntity($object);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function supportsTransformation($data, string $to, array $context = []): bool
+    {
+        if ($data instanceof Invoice) {
+            return false;
+        }
+        return Invoice::class === $to && null !== ($context['input']['class'] ?? null);
+
+    }
+}
