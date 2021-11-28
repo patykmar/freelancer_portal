cf906a1ada01b5640cf51b0426f211a17d665872
Ing. Martin Patyk <martin@patyk.cz>
2021-11-28 17:57:19 +0100

    added new fields $logs and $childTickets

--- src/Dto/Out/TicketDtoOut.php
+++ src/Dto/Out/TicketDtoOut.php
@@ -5,10 +5,10 @@ namespace App\Dto\Out;
 class TicketDtoOut
 {
     public ?int $id = 0;
-    public array $serviceCatalog = array();
-    public array $ci = array();
-    public array $queue_user = array();
-    public array $userCreated = array();
+    public array $serviceCatalog = [];
+    public array $ci = [];
+    public array $queue_user = [];
+    public array $userCreated = [];
     public int $ticketState = 0;
     public int $ticketType = 0;
     public int $priority = 0;
@@ -17,7 +17,7 @@ class TicketDtoOut
     public string $descriptionBody = '';
     public int $createdDatetime = 0;
     public string $toString = '';
-    public array $parentTicket = array();
+    public array $parentTicket = [];
     public ?int $userResolved = 0;
     public ?int $workInventory = 0;
     public ?int $ticketCloseState = 0;
@@ -25,4 +25,6 @@ class TicketDtoOut
     public ?int $closedDatetime = 0;
     public ?int $reactionDatetime = 0;
     public ?int $deliveryDatetime = 0;
+    public array $logs = [] ;
+    public array $childTickets = [] ;
 }
