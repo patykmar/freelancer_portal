e51118e92933472d459ef71acd9dc4d394a74f10
Ing. Martin Patyk <martin@patyk.cz>
2021-11-30 17:57:25 +0100

    init basic test

--- tests/api/CompanyApiTest.php
+++ tests/api/CompanyApiTest.php
@@ -3,12 +3,28 @@
 namespace App\Tests\api;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class CompanyApiTest extends ApiTestCase
 {
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function testCompanies(): void
     {
-        $response = static::createClient()->request('GET','/');
+        $options = [
+            'auth_basic' => null,
+            'auth_bearer' => null,
+            'query' => [],
+            'headers' => ['accept' => ['application/ld+json']],
+            'body' => '',
+            'json' => null,
+            'base_uri' => 'https://127.0.0.1:8000',
+            'extra' => [],
+        ];
+
+        $response = static::createClient()->request('GET','/api/companies', $options);
+        dump($response);
     }
 }
