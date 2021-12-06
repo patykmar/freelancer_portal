f76e827eec16e222373d7e9b67317be4ed476464
Martin Patyk <martin@patyk.cz>
2021-12-06 15:12:28 +0100

    check if hydra:member is not empty

--- tests/api/CompanyApiTest.php
+++ tests/api/CompanyApiTest.php
@@ -3,12 +3,19 @@
 namespace App\Tests\api;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class CompanyApiTest extends ApiTestCase
 {
 
     /**
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
      * @throws TransportExceptionInterface
      */
     public function testCompanies(): void
@@ -24,7 +31,10 @@ class CompanyApiTest extends ApiTestCase
             'extra' => [],
         ];
 
+        /** @var ResponseInterface $response */
         $response = static::createClient()->request('GET','/api/companies', $options);
-        dump($response);
+
+        $jsonData = json_decode($response->getContent(), true);
+        self::assertNotEmpty($jsonData['hydra:member']);
     }
 }
