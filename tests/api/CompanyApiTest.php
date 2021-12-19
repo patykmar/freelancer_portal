02e0a275d770776f5797b55765023c7430cec20f
Martin Patyk <martin@patyk.cz>
2021-12-19 14:59:30 +0100

    moved test methods to Trait

--- tests/api/CompanyApiTest.php
+++ tests/api/CompanyApiTest.php
@@ -3,14 +3,11 @@
 namespace App\Tests\api;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-use function PHPUnit\Framework\assertEquals;
 
 class CompanyApiTest extends ApiTestCase implements ApiTestInterface
 {
+    use ApiTestTrait;
+
     private const URL = '/api/companies';
     private const BASE_URI = 'https://127.0.0.1:8000';
 
@@ -24,7 +21,6 @@ class CompanyApiTest extends ApiTestCase implements ApiTestInterface
         "zipCode" => "530 03",
         "country" => 1
     ];
-
     private array $bodyChanged = [
         "name" => "F@XC@NN CZ sro",
         "description" => "electronicsConsumer ",
@@ -35,126 +31,4 @@ class CompanyApiTest extends ApiTestCase implements ApiTestInterface
         "zipCode" => "555 77",
         "country" => 3
     ];
-
-    /**
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @return int
-     */
-    public function testPost(): int
-    {
-        $response = self::createClient()
-            ->request('POST', self::URL, [
-                'json' => $this->body,
-                'base_uri' => self::BASE_URI
-            ]);
-
-        $this->assertResponseIsSuccessful();
-
-        $content = json_decode($response->getContent(), true);
-
-        assertEquals($this->body['name'], $content['name']);
-        assertEquals($this->body['description'], $content['description']);
-        assertEquals($this->body['companyId'], $content['companyId']);
-        assertEquals($this->body['vatNumber'], $content['vatNumber']);
-        assertEquals($this->body['street'], $content['street']);
-        assertEquals($this->body['city'], $content['city']);
-        assertEquals($this->body['zipCode'], $content['zipCode']);
-        assertEquals($this->body['country'], $content['country']['id']);
-
-        return $content['id'];
-    }
-
-    /**
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @depends testPost
-     */
-    public function testGetAll(): void
-    {
-        $content = self::createClient()
-            ->request('GET', self::URL)
-            ->getContent();
-        $jsonData = json_decode($content, true);
-        $this->assertNotEmpty($jsonData['hydra:member']);
-    }
-
-    /**
-     * @depends testPost
-     * @param int $id
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     */
-    public function testGetItem(int $id): void
-    {
-        $response = $this->createClient()
-            ->request('GET', self::URL . '/' . $id, [
-                'base_uri' => self::BASE_URI
-            ]);
-
-        $this->assertResponseIsSuccessful();
-
-        $content = json_decode($response->getContent(), true);
-
-        assertEquals($this->body['name'], $content['name']);
-        assertEquals($this->body['description'], $content['description']);
-        assertEquals($this->body['companyId'], $content['companyId']);
-        assertEquals($this->body['vatNumber'], $content['vatNumber']);
-        assertEquals($this->body['street'], $content['street']);
-        assertEquals($this->body['city'], $content['city']);
-        assertEquals($this->body['zipCode'], $content['zipCode']);
-        assertEquals($this->body['country'], $content['country']['id']);
-    }
-
-    /**
-     * @param int $id
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @depends testPost
-     */
-    public function testPut(int $id): void
-    {
-        $response = $this->createClient()
-            ->request('PUT', self::URL . '/' . $id, [
-                'json' => $this->bodyChanged,
-                'base_uri' => self::BASE_URI
-            ]);
-
-        $this->assertResponseIsSuccessful();
-
-        $content = json_decode($response->getContent(), true);
-
-        assertEquals($this->bodyChanged['name'], $content['name']);
-        assertEquals($this->bodyChanged['description'], $content['description']);
-        assertEquals($this->bodyChanged['companyId'], $content['companyId']);
-        assertEquals($this->bodyChanged['vatNumber'], $content['vatNumber']);
-        assertEquals($this->bodyChanged['street'], $content['street']);
-        assertEquals($this->bodyChanged['city'], $content['city']);
-        assertEquals($this->bodyChanged['zipCode'], $content['zipCode']);
-        assertEquals($this->bodyChanged['country'], $content['country']['id']);
-    }
-
-    /**
-     * @param int $id
-     * @return void
-     * @throws TransportExceptionInterface
-     * @depends testPost
-     */
-    public function testDelete(int $id): void
-    {
-        $response = $this->createClient()
-            ->request('DELETE', self::URL . '/' . $id, [
-                'base_uri' => self::BASE_URI
-            ]);
-        $this->assertResponseIsSuccessful();
-        $this->assertEquals(204, $response->getStatusCode());
-    }
 }
--- tests/api/CountryTestApi.php
+++ tests/api/CountryTestApi.php
@@ -3,13 +3,11 @@
 namespace App\Tests\api;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class CountryTestApi extends ApiTestCase implements ApiTestInterface
 {
+    use ApiTestTrait;
+
     private const URL = '/api/countries';
     private const BASE_URI = 'https://127.0.0.1:8000';
 
@@ -17,109 +15,8 @@ class CountryTestApi extends ApiTestCase implements ApiTestInterface
         "name" => "United Kingdom of Great Britain and Northern Ireland",
         "iso3166alpha3" => "GBR"
     ];
-
     private array $bodyChanged = [
         "name" => "Northern Kingdom of Great Britain and United  Ireland",
         "iso3166alpha3" => "GBR"
     ];
-
-    /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
-     */
-    function testGetAll(): void
-    {
-        $content = self::createClient()
-            ->request('GET', self::URL)
-            ->getContent();
-        $jsonData = json_decode($content, true);
-        $this->assertNotEmpty($jsonData['hydra:member']);
-    }
-
-    /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
-     * @return int
-     */
-    function testPost(): int
-    {
-        $response = $this->createClient()
-            ->request("POST", self::URL, [
-               'json' => $this->body,
-               'base_uri' => self::BASE_URI
-            ]);
-
-        $this->assertResponseIsSuccessful();
-
-        $content = json_decode($response->getContent(), true);
-
-        $this->assertEquals($this->body['name'], $content['name']);
-        $this->assertEquals($this->body['iso3166alpha3'], $content['iso3166alpha3']);
-
-        return $content['id'];
-    }
-
-    /**
-     * @param int $id
-     * @throws ClientExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws TransportExceptionInterface
-     * @depends testPost
-     */
-    function testGetItem(int $id): void
-    {
-        $response = $this->createClient()
-            ->request('GET', self::URL . '/' . $id, [
-                'base_uri' => self::BASE_URI
-            ]);
-
-        $this->assertResponseIsSuccessful();
-
-        $content = json_decode($response->getContent(), true);
-
-        $this->assertEquals($this->body['name'], $content['name']);
-        $this->assertEquals($this->body['iso3166alpha3'], $content['iso3166alpha3']);
-    }
-
-    /**
-     * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
-     * @depends testPost
-     */
-    function testPut(int $id): void
-    {
-        $response = $this->createClient()
-            ->request('PUT', self::URL . '/' . $id, [
-                'json' => $this->bodyChanged,
-                'base_uri' => self::BASE_URI
-            ]);
-
-        $this->assertResponseIsSuccessful();
-
-        $content = json_decode($response->getContent(), true);
-
-        $this->assertEquals($this->bodyChanged['name'], $content['name']);
-        $this->assertEquals($this->bodyChanged['iso3166alpha3'], $content['iso3166alpha3']);
-    }
-
-    /**
-     * @throws TransportExceptionInterface
-     * @depends testPost
-     */
-    function testDelete(int $id): void
-    {
-        $response = $this->createClient()
-            ->request('DELETE', self::URL . '/' . $id, [
-                'base_uri' => self::BASE_URI
-            ]);
-        $this->assertResponseIsSuccessful();
-        $this->assertEquals(204, $response->getStatusCode());
-    }
 }
